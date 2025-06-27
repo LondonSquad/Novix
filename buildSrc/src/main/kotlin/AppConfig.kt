1d07b1decd95b1fb57ffce9830a1f2354968044a
Ahmed Mones <68463985+Al-Taie@users.noreply.github.com>
2025-06-27 20:43:43 +0300

    integrate build logic and configuration files

---
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -0,0 +1,22 @@
+package com.london.buildscr
+
+import org.gradle.api.JavaVersion
+
+object AppConfig {
+
+    const val ENABLE_R8_FULL_MODE: Boolean = true
+    const val IS_RELEASE_MODE_DEBUGGABLE: Boolean = false
+
+    object Version {
+        const val MIN_SDK = 26
+        const val TARGET_SDK = 34
+        const val COMPILE_SDK = 35
+        val JVM = JavaVersion.VERSION_17
+        const val BUILD_TOOLS = "35.0.0"
+    }
+
+    private const val APPLICATION_ID_GROUP = "com.london"
+    private const val APPLICATION_ID_SUFFIX = "novix"
+    const val APPLICATION_ID = "${APPLICATION_ID_GROUP}.${APPLICATION_ID_SUFFIX}"
+    const val ANDROID_TEST_INSTRUMENTATION = "androidx.test.runner.AndroidJUnitRunner"
+}
