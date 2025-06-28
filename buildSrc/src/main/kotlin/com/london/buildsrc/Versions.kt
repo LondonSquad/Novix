3f885b7f2869ea8868a0e943a3a592aa7e5b67fa
AsmaaKhaledGomaa <asmaa.kh.gomaa@gmail.com>
2025-06-28 16:22:19 +0300

    refactor: Relocate buildSrc files and add package

---
+++ buildSrc/src/main/kotlin/com/london/buildsrc/Versions.kt
@@ -0,0 +1,37 @@
+package com.london.buildsrc
+
+object Versions {
+    const val CORE_KTX = "1.16.0"
+    const val LIFECYCLE_RUNTIME_KTX = "2.9.1"
+    const val ACTIVITY_COMPOSE = "1.10.1"
+    const val SPLASH_SCREEN = "1.0.1"
+
+    const val COMPOSE_BOM = "2025.06.01"
+    const val COMPOSE_UI = "1.8.3"
+    const val MATERIAL3 = "1.3.2"
+    const val NAVIGATION_COMPOSE = "2.9.0"
+
+    const val ROOM = "2.7.2"
+
+    const val DATASTORE = "1.1.7"
+
+    const val KOIN = "4.1.0"
+    const val KOIN_ANNOTATIONS = "1.4.0"
+
+    const val KTOR = "2.3.13"
+
+    const val COROUTINES = "1.10.2"
+    const val COROUTINES_TEST = "1.8.1"
+    const val DATETIME = "0.6.0"
+
+    const val COIL = "2.7.0"
+    const val LOTTIE = "4.0.0"
+    const val REMEMBER_PREFERENCE = "1.1.1"
+
+    const val JUNIT = "4.13.2"
+    const val JUNIT_JUPITER = "5.12.2"
+    const val JUNIT_EXT = "1.2.1"
+    const val ESPRESSO = "3.6.1"
+    const val MOCKK = "1.14.4"
+    const val TRUTH = "1.4.4"
+}
