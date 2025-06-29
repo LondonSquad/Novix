d6dbdce18dfd3c6e3e6f248f9045b40ae720eb8b
abdelrhman1002 <abdelrhmanshabana999@gmail.com>
2025-06-29 05:05:05 +0300

    refactor ActorItem layout and drawing border for card

--- app/src/main/java/com/london/novix/presentation/component/ActorItem.kt
+++ app/src/main/java/com/london/novix/presentation/component/ActorItem.kt
@@ -1,6 +1,5 @@
 package com.london.novix.presentation.component
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -15,6 +14,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.london.novix.R
@@ -23,18 +24,13 @@ import com.london.novix.presentation.designSystem.theme.ThemePreviews
 
 @Composable
 fun ActorItem(
-    actorName: String,
-    characterName: String?,
-    imageRes: Any,
-    modifier: Modifier = Modifier
+    actorName: String, characterName: String?, imageRes: Any, modifier: Modifier = Modifier
 ) {
     Row(
-        modifier = modifier
-            .fillMaxWidth(),
-        verticalAlignment = Alignment.CenterVertically
+        modifier = modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically
     ) {
         AsyncImage(
-            model = imageRes,
+            model = "",
             contentDescription = null,
             modifier = Modifier
                 .size(78.dp)
@@ -43,28 +39,50 @@ fun ActorItem(
                         topStart = 12.dp, topEnd = 12.dp, bottomStart = 12.dp
                     )
                 )
-                .border(width = 1.dp, color = NovixTheme.colors.stroke),
+                .border(
+                    shape = RoundedCornerShape(
+                        topStart = 12.dp, topEnd = 12.dp, bottomStart = 12.dp
+                    ), width = 1.dp, color = NovixTheme.colors.stroke
+                ),
         )
+        val color = NovixTheme.colors.stroke
         Column(
             modifier = Modifier
                 .align(Alignment.Bottom)
                 .fillMaxWidth()
                 .defaultMinSize(minHeight = 55.dp)
-                .border(width = 1.dp, color = NovixTheme.colors.stroke)
-                .padding(start = 8.dp),
-            verticalArrangement = if (characterName == null) Arrangement.Center
-            else Arrangement.Top,
+                .drawBehind {
+                    val strokeWidth = 1.dp.toPx()
+                    drawLine(
+                        color = color,
+                        start = Offset(0f, 0f),
+                        end = Offset(size.width, 0f),
+                        strokeWidth = strokeWidth
+                    )
+                    drawLine(
+                        color = color,
+                        start = Offset(0f, size.height - 1),
+                        end = Offset(size.width, size.height),
+                        strokeWidth = strokeWidth
+                    )
+                    drawLine(
+                        color = color,
+                        start = Offset(size.width, 0f),
+                        end = Offset(size.width, size.height),
+                        strokeWidth = strokeWidth
+                    )
+                }
+                .padding(start = 8.dp, top = 4.5.dp, bottom = 4.5.dp),
+            verticalArrangement = Arrangement.Center,
         ) {
             Text(
                 text = actorName,
                 style = NovixTheme.typography.title.medium,
                 color = NovixTheme.colors.body,
-                modifier = Modifier.padding(top = if (characterName == null) 0.dp else 4.dp)
             )
             characterName?.let {
                 Text(
                     text = it,
-                    modifier = Modifier.padding(bottom = 4.5.dp),
                     style = NovixTheme.typography.label.small,
                     color = NovixTheme.colors.hint,
                 )
