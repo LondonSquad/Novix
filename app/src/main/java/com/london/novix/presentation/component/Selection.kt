5ed98157f8965bb2b87882dfe7383223ddac840c
Marwan Qashwa <marawankashwa24@gmail.com>
2025-06-29 04:41:23 +0300

    fix: enhance text align

--- app/src/main/java/com/london/novix/presentation/component/Selection.kt
+++ app/src/main/java/com/london/novix/presentation/component/Selection.kt
@@ -4,6 +4,7 @@ import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -13,7 +14,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.london.novix.presentation.designSystem.theme.NovixTheme
 import com.london.novix.presentation.designSystem.theme.ThemePreviews
@@ -34,29 +34,28 @@ fun Selection(
         if (isSelected) NovixTheme.colors.primary
         else NovixTheme.colors.stroke
     )
+    val borderWidth = if (isSelected) (1.5).dp else 1.dp
 
     Row(
         modifier = modifier
             .clip(RoundedCornerShape(12.dp))
             .background(color = backgroundColor)
-            .border(width = (1.5).dp, color = borderColor, shape = RoundedCornerShape(12.dp))
+            .border(width = borderWidth, color = borderColor, shape = RoundedCornerShape(12.dp))
             .clickable(onClick = onClick, indication = null, interactionSource = null)
             .padding(horizontal = 12.dp, vertical = 16.dp),
-        verticalAlignment = Alignment.CenterVertically
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
     ) {
         Text(
             text = mainText,
             style = NovixTheme.typography.label.large,
             color = NovixTheme.colors.body,
-            textAlign = TextAlign.Start,
-            modifier = Modifier.weight(1f)
         )
         subText?.let {
             Text(
                 text = subText,
                 style = NovixTheme.typography.label.small,
                 color = NovixTheme.colors.hint,
-                textAlign = TextAlign.End,
             )
         }
     }
