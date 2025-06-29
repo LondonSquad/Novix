0fd8b06c917fe768518bb3058baba4c7fc3e6ac6
Abdulrahman1890 <Abdulrahmanmm2011@hotmail.com>
2025-06-29 03:27:59 +0300

    comp: section header implementation

---
+++ app/src/main/java/com/london/novix/presentation/component/SectionHeader.kt
@@ -0,0 +1,66 @@
+package com.london.novix.presentation.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.london.novix.R
+import com.london.novix.presentation.designSystem.theme.NovixTheme
+import com.london.novix.presentation.designSystem.theme.ThemePreviews
+import com.london.novix.presentation.designSystem.theme.noRippleClickable
+
+@Composable
+fun SectionHeader(
+    text: String,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = {}
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Text(
+            text = text,
+            style = NovixTheme.typography.headline.small,
+            color = NovixTheme.colors.title,
+        )
+        Row(
+            modifier = Modifier
+                .align(Alignment.CenterVertically)
+                .noRippleClickable(onClick),
+        ) {
+            Text(
+                text = stringResource(R.string.all),
+                style = NovixTheme.typography.label.medium,
+                color = NovixTheme.colors.primary,
+                modifier = Modifier.padding(end = 4.dp)
+            )
+            Icon(
+                painter = painterResource(id = R.drawable.icon_right_arrow),
+                contentDescription = "right arrow",
+                tint = NovixTheme.colors.primary
+            )
+        }
+    }
+}
+
+@ThemePreviews
+@Composable
+fun SectionHeaderPreview() {
+    NovixTheme {
+        SectionHeader(
+            text = stringResource(R.string.new_arrival)
+        )
+    }
+}
