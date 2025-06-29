c9c8b94fc009e8a3aa493e25f679d7aefdfdd6f4
Abdulrahman1890 <Abdulrahmanmm2011@hotmail.com>
2025-06-29 03:33:20 +0300

    comp: section header implementation

--- app/src/main/java/com/london/novix/presentation/component/SectionHeader.kt
+++ app/src/main/java/com/london/novix/presentation/component/SectionHeader.kt
@@ -2,10 +2,8 @@ package com.london.novix.presentation.component
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
