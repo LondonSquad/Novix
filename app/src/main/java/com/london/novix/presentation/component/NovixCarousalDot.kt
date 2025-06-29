40398e2307df4b05210a455923f079dedce0a148
ahmed samy <ahmbdelkhalek@gmail.com>
2025-06-29 11:02:01 +0300

    comp: add CarousalDot component

---
+++ app/src/main/java/com/london/novix/presentation/component/NovixCarousalDot.kt
@@ -0,0 +1,83 @@
+package com.london.novix.presentation.component
+
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.london.novix.presentation.designSystem.theme.NovixTheme
+import com.london.novix.presentation.designSystem.theme.ThemePreviews
+
+@Composable
+fun NovixCarousalRow(
+    dotsStates: List<Boolean>,
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(4.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        items(dotsStates) { dot ->
+            NovixCarousalDot(
+                isSelected = dot
+            )
+        }
+    }
+}
+
+@Composable
+private fun NovixCarousalDot(
+    isSelected: Boolean,
+    modifier: Modifier = Modifier
+) {
+    val dotTintColor by animateColorAsState(
+        targetValue = if (isSelected) NovixTheme.colors.primary else NovixTheme.colors.hint,
+        animationSpec = tween(400)
+    )
+
+    val dotSize by animateDpAsState(
+        targetValue = if (isSelected) 8.dp else 5.dp,
+        animationSpec = tween(400)
+    )
+
+    Box(
+        modifier = modifier
+            .background(
+                color = dotTintColor,
+                shape = CircleShape
+            )
+            .size(dotSize)
+            .then(
+                if (!isSelected)
+                    Modifier.border(
+                        width = 0.5.dp,
+                        color = NovixTheme.colors.onPrimaryHint,
+                        shape = CircleShape
+                    ) else Modifier
+            )
+    )
+}
+
+@ThemePreviews
+@Composable
+fun PreviewCarousalDot() {
+    NovixTheme {
+        NovixCarousalRow(
+            dotsStates = listOf(false, true, false, false, false, false, false, false),
+            modifier = Modifier.width(44.dp)
+        )
+    }
+}
