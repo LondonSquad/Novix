9b1dec93d00971de949b971bda283b5db2f1141d
ahmed samy <ahmbdelkhalek@gmail.com>
2025-06-30 14:58:17 +0300

    feat: implement the Performance and analytics and crashlytics

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,9 @@ plugins {
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.google.firebase.firebase.perf) apply false
+    alias(libs.plugins.google.gms.google.services) apply false
+    alias(libs.plugins.google.firebase.crashlytics) apply false
 }
 
 configureGitHooks()
