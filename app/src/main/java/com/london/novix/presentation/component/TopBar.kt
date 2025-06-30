aaac16f83d853fdd5b111fdf74c09e05223554c1
Mostafa Mohammed <tofymofy.55@gmail.com>
2025-06-30 20:41:26 +0300

    feat: Add TopBar component and icons
    This commit introduces a new `TopBar` composable function and its associated icons: `add_icon.xml`, `arrow_left.xml`, `icondesign.xml`, and `pencil_edit.xml`.
    
    The `TopBar` component provides different variations based on the `TopBarType` enum, allowing for default, app bar, one-option, two-option, without back arrow, and without screen title configurations.
    
    The new icons are used within the `TopBar` component for navigation and actions.

---
+++ app/src/main/java/com/london/novix/presentation/component/TopBar.kt
@@ -0,0 +1,149 @@
+package com.london.novix.presentation.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.london.novix.R
+import com.london.novix.presentation.designSystem.theme.NovixTheme
+
+@Composable
+fun TopBar(
+    type: TopBarType = TopBarType.Default,
+    title: String = "My account",
+    onBackClick: (() -> Unit)?,
+    option1: (() -> Unit)? = null,
+    option2: (() -> Unit)? = null
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 12.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        when (type) {
+            TopBarType.Default -> {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Image(
+                        painter = painterResource(R.drawable.icondesign),
+                        contentDescription = "Logo",
+                        modifier = Modifier.padding(end = 12.dp)
+                    )
+                    Column {
+                        Text(
+                            text = "Novix",
+                            style = NovixTheme.typography.title.medium,
+                            color = NovixTheme.colors.body
+                        )
+                        Text(
+                            text = "Born from Nova, Made for Flix.",
+                            style = NovixTheme.typography.label.small,
+                            color = NovixTheme.colors.hint
+                        )
+                    }
+                }
+            }
+
+            else -> {
+                if (type != TopBarType.WithoutBackArrow) {
+                    ButtonTopBar(icon = R.drawable.arrow_left, onClick = { onBackClick })
+                }
+            }
+        }
+
+        if (type != TopBarType.Default && type != TopBarType.WithoutScreenTitle) {
+            Text(
+                text = title,
+                style = NovixTheme.typography.title.large,
+                color = NovixTheme.colors.body,
+                modifier = Modifier.weight(1f).padding(horizontal = 4.dp)
+            )
+        } else if (type == TopBarType.WithoutScreenTitle) {
+            Spacer(modifier = Modifier.weight(1f))
+        }
+
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            if (type == TopBarType.WithOneOption) {
+                option1?.let {
+                    ButtonTopBar(R.drawable.add_icon, onClick = it)
+                }
+            }
+            if (type == TopBarType.WithTwoOptions) {
+                option1?.let {
+                    ButtonTopBar(R.drawable.add_icon, onClick = it)
+                }
+                option2?.let {
+                    ButtonTopBar(R.drawable.pencil_edit, onClick = it)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ButtonTopBar(icon: Int, onClick: () -> Unit) {
+    IconButton(
+        onClick = onClick,
+        modifier = Modifier.padding(end = 8.dp)
+            .size(40.dp)
+            .clip(RoundedCornerShape(12.dp))
+            .background(NovixTheme.colors.iconBackgroundLow)
+            .border(
+                width = 1.dp,
+                color = NovixTheme.colors.stroke,
+                shape = RoundedCornerShape(12.dp)
+            )
+    ) {
+        Icon(
+            painter =  painterResource(icon),
+            contentDescription = "Back",
+            tint = NovixTheme.colors.title
+        )
+    }
+}
+
+enum class TopBarType {
+    Default,
+    AppBar,
+    WithOneOption,
+    WithTwoOptions,
+    WithoutBackArrow,
+    WithoutScreenTitle
+}
+
+
+@Composable
+@Preview(
+    showBackground = true,
+    name = "TopBar - Dark Theme",
+)
+fun TopBarPreview() {
+    TopBar(
+        type = TopBarType.WithTwoOptions,
+        onBackClick = {},
+        option1 = {},
+        option2 = {}
+    )
+}
