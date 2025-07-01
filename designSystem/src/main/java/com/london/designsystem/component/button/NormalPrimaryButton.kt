66e4a4a45b1792f1ea5f116571c6543574a5a84b
Bassant Mohammed <bassant.22230@gmail.com>
2025-07-01 19:06:55 +0300

    fix: resolve comments in pr

--- designSystem/src/main/java/com/london/designsystem/component/button/NormalPrimaryButton.kt
+++ designSystem/src/main/java/com/london/designsystem/component/button/NormalPrimaryButton.kt
@@ -15,13 +15,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.london.designsystem.R
 import com.london.designsystem.theme.NovixTheme
 import com.london.designsystem.theme.ThemePreviews
-import com.london.designsystem.typography.NovixFont
 
 
 @Composable
