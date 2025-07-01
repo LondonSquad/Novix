1be3ab58dcdc261a34184021ffb965b6f4ddecc5
Bassant Mohammed <102248144+BassantM@users.noreply.github.com>
2025-07-01 19:28:44 +0300

    feature/component/primary-button (#46)
    add primary buttons

--- designSystem/build.gradle.kts
+++ designSystem/build.gradle.kts
@@ -42,4 +42,7 @@ dependencies {
     debugImplementation(libs.bundles.compose.debug)
     androidTestImplementation(libs.bundles.base.testing)
     testImplementation(libs.bundles.testing)
+
+    //lottie animation
+    implementation(libs.lottie.compose)
 }
