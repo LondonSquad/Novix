b0e90f6f08ce088b34815f8160f89cb3d79dde71
Aseel AbuKmail <aseelabukmail@gmail.com>
2025-07-02 15:32:26 +0300

    fix: save icon component
    Co-authored-by: Karim Al-Zmzmy <81706918+karimdeabes@users.noreply.github.com>

--- designSystem/src/main/java/com/london/designsystem/component/SaveIcon.kt
+++ designSystem/src/main/java/com/london/designsystem/component/SaveIcon.kt
@@ -5,19 +5,18 @@ import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -25,6 +24,7 @@ import androidx.compose.ui.unit.dp
 import com.london.designsystem.R
 import com.london.designsystem.theme.NovixTheme
 import com.london.designsystem.theme.ThemePreviews
+import com.london.designsystem.theme.noRippleClickable
 
 @Composable
 fun SaveIcon(
@@ -40,41 +40,38 @@ fun SaveIcon(
             stiffness = 100f
         )
     )
-
-    IconButton(
-        onClick = onSaveClick,
+    Box (
         modifier = modifier
             .size(32.dp)
+            .clip(RoundedCornerShape(8.dp))
             .background(color = backgroundColor)
             .border(
                 width = 1.dp,
                 color = NovixTheme.colors.stroke,
                 shape = RoundedCornerShape(8.dp)
             )
+            .padding(6.dp)
+            .noRippleClickable{
+                onSaveClick()
+            }
     ) {
-        Box(
-            contentAlignment = Alignment.Center,
-            modifier = Modifier.fillMaxSize(),
+        Icon(
+            painter = painterResource(R.drawable.icon_remove),
+            contentDescription = "Not Save",
+            tint = NovixTheme.colors.strokeHigh,
+            modifier = Modifier
+                .scale(1f - animatedProgress)
+                .alpha(1f - animatedProgress)
+        )
 
-            ) {
-            Icon(
-                painter = painterResource(R.drawable.icon_remove),
-                contentDescription = "Not Save",
-                tint = NovixTheme.colors.strokeHigh,
-                modifier = Modifier
-                    .scale(1f - animatedProgress)
-                    .alpha(1f - animatedProgress)
-            )
-
-            Icon(
-                painter = painterResource(R.drawable.icon_save),
-                contentDescription = "Save",
-                tint = NovixTheme.colors.strokeHigh,
-                modifier = Modifier
-                    .scale(animatedProgress)
-                    .alpha(animatedProgress)
-            )
-        }
+        Icon(
+            painter = painterResource(R.drawable.icon_save),
+            contentDescription = "Save",
+            tint = NovixTheme.colors.strokeHigh,
+            modifier = Modifier
+                .scale(animatedProgress)
+                .alpha(animatedProgress)
+        )
     }
 }
 
@@ -82,7 +79,7 @@ fun SaveIcon(
 @Composable
 fun SaveIconPreview() {
     NovixTheme {
-        var isSaved by remember { mutableStateOf(false) }
+        var isSaved by remember { mutableStateOf(true) }
         SaveIcon(
             isSaved = isSaved,
             onSaveClick = { isSaved = !isSaved }
