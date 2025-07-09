fed6476837400fb15a4df71f55fcd9bf8108f44c
Aseel AbuKmail <aseelabukmail@gmail.com>
2025-07-09 15:22:00 +0300

    Feature/setup api (#62)
    * feature: add serialization plugins in gradle - data
    
    * feature: add internet permission in manifest - presentation
    
    * feature: add api constants object
    
    * feature: add search response dto for movies, tvs, and actors
    
    * feature: add remote data source and it's implementation
    
    * feature: add search repository  implementation file
    
    * feature: use a place holder for api key

---
+++ data/src/main/java/com/london/data/datasource/remote/ApiConstants.kt
@@ -0,0 +1,8 @@
+package com.london.data.datasource.remote
+
+object ApiConstants {
+    const val SEARCH_HOST = "api.themoviedb.org"
+    const val SEARCH_PATH_MOVIES = "3/search/movie"
+    const val SEARCH_PATH_TVS = "3/search/tv"
+    const val SEARCH_PATH_ACTORS = "3/search/person"
+}
