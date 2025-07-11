a076e3127b24cf31d6f9d2866e11a0370288f9d8
ahmeddsamii <154278495+ahmeddsamii@users.noreply.github.com>
2025-07-11 19:09:56 +0300

    Feature/database room (#64)
    * feat: implement local data source for search functionality
    
    * refactor: Rename LocalDataSourceImplExpansion to \LocalDataSourceExpansion
    
    * feat: Add DI for database and LocalDataSource tests
    
    * refactor: Migrate Room from kapt to KSP
    
    * feat: Configure Room schema location and Proguard rules
    
    * chore: Disable minification for data module
    
    * Refactor: Rename local DTOs for clarity
    
    * test: Includes LocalDataSourceImpl into coverage
    
    * refactor: Relocate DTOs
    
    * feat: Implement get search by query for local data source
    
    * test: add tests for get by query methods in LocalDataSourceImpl
    
    * refactor: Update property names in response DTOs
    
    * fix: Add missing SerialName annotations for search DTOs
    
    * refactor: Separate local and remote DTOs and update DAO interfaces
    
    * refactor: restructure query expansion classes
    
    * refactor: make name more clear
    
    * fix: remove the test variable with SearchDaoHandlerImpl that was the cause of the compile error
    
    * refactor: update exception handling in data source
    
    * refactor: make getAll function to return the list instead of single object
    
    * refactor: wrap dao responses in lists
    
    ---------
    
    Co-authored-by: abdelrhman1002 <abdelrhmanshabana999@gmail.com>

---
+++ data/src/main/java/com/london/data/datasource/local/model/SearchTvShowDtoLocal.kt
@@ -0,0 +1,32 @@
+package com.london.data.datasource.local.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "search_tv_shows_table")
+data class SearchTvShowLocal(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val date: Long = System.currentTimeMillis(),
+    val query: String,
+    val page: Int,
+    val results: List<SearchTvShowDtoLocal>,
+    val totalPages: Int,
+    val totalResults: Int
+)
+
+data class SearchTvShowDtoLocal(
+    val adult: Boolean,
+    val backdropPath: String,
+    val genreIds: List<Int>,
+    val id: Int,
+    val originCountry: List<String>,
+    val originalLanguage: String,
+    val originalName: String,
+    val overview: String,
+    val popularity: Int,
+    val posterPath: String,
+    val firstAirDate: String,
+    val name: String,
+    val voteAverage: Int,
+    val voteCount: Int
+)
