4d2f60038576a1be9ba68c53023065a9a2483c42
Abdulrahman1890 <Abdulrahmanmm2011@hotmail.com>
2025-07-11 21:05:58 +0300

    fix: using painterResource instead of painter

--- app/src/main/java/com/london/app/navigation/NavigationHelper.kt
+++ app/src/main/java/com/london/app/navigation/NavigationHelper.kt
@@ -1,6 +1,7 @@
 package com.london.app.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import com.london.designsystem.R
 import com.london.designsystem.component.NavigationTab
 import com.london.designsystem.utils.painter
@@ -8,31 +9,33 @@ import com.london.designsystem.utils.painter
 class NavigationHelper {
     companion object {
         @Composable
-        fun getNavigationTabs(): List<NavigationTab> {
+        fun getNavigationTabs(
+
+        ): List<NavigationTab> {
             return listOf(
                 NavigationTab(
-                    idleIcon = R.drawable.icon_home.painter,
-                    selectedIcon = R.drawable.icon_home_filled.painter,
+                    idleIcon = painterResource(R.drawable.icon_home),
+                    selectedIcon = painterResource(R.drawable.icon_home_filled),
                     route = "home"
                 ),
                 NavigationTab(
-                    idleIcon = R.drawable.icon_search.painter,
-                    selectedIcon = R.drawable.icon_search_filled.painter,
+                    idleIcon = painterResource(R.drawable.icon_search),
+                    selectedIcon = painterResource(R.drawable.icon_search_filled),
                     route = "search"
                 ),
                 NavigationTab(
-                    idleIcon = R.drawable.icon_masks.painter,
-                    selectedIcon = R.drawable.icon_masks_filled.painter,
+                    idleIcon = painterResource(R.drawable.icon_masks),
+                    selectedIcon = painterResource(R.drawable.icon_masks_filled),
                     route = "categories"
                 ),
                 NavigationTab(
-                    idleIcon = R.drawable.icon_bookmark.painter,
-                    selectedIcon = R.drawable.icon_bookmark_filled.painter,
+                    idleIcon = painterResource(R.drawable.icon_bookmark),
+                    selectedIcon = painterResource(R.drawable.icon_bookmark_filled),
                     route = "bookmarks"
                 ),
                 NavigationTab(
-                    idleIcon = R.drawable.icon_user.painter,
-                    selectedIcon = R.drawable.icon_user_filled.painter,
+                    idleIcon = painterResource(R.drawable.icon_user),
+                    selectedIcon = painterResource(R.drawable.icon_user_filled),
                     route = "account"
                 )
             )
