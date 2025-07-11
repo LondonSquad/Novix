5b4a61e02e13bf6607981cccba6395d2c84258c7
AsmaaKhaledGomaa <asmaa.kh.gomaa@gmail.com>
2025-07-11 12:44:15 +0300

    feat: change `searchQuery` type from `String` to `TextFieldValue` in `SearchUiState`

--- presentation/src/main/java/com/london/presentation/screen/search/SearchUiState.kt
+++ presentation/src/main/java/com/london/presentation/screen/search/SearchUiState.kt
@@ -1,14 +1,15 @@
 package com.london.presentation.screen.search
 
+import androidx.compose.ui.text.input.TextFieldValue
+
 data class SearchUiState(
-    val searchQuery: String = "",
+    val searchQuery: TextFieldValue = TextFieldValue(""),
     val recentViewed: List<String> = emptyList(),
     val recentSearches: List<String> = emptyList(),
     val isLoading: Boolean = false,
     val searchResults: List<SearchResult> = emptyList(),
     val isSearching: Boolean = false
 )
-
 
 data class SearchResult(
     val id: String,
