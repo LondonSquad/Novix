5807509b090e1b4be6b2cbd3256a8c7f9a35e3fb
Abdulrahman1890 <Abdulrahmanmm2011@hotmail.com>
2025-07-11 22:35:39 +0300

    fix: delete unnecessary spaces

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,13 +67,9 @@ dependencies {
     implementation(libs.firebase.crashlytics)
     implementation(libs.firebase.analytics)
     testImplementation(libs.bundles.testing)
-
-
     implementation(libs.androidx.navigation.compose)
     implementation (libs.koin.androidx.navigation)
     implementation (libs.androidx.material)
-
-
     implementation(libs.bundles.room)
     implementation(libs.bundles.koin)
     ksp(libs.bundles.room.ksp)
