f21ee174a776d912cdbabd68304e355e92d3b92e
Mostafa Mohammed <tofymofy.55@gmail.com>
2025-07-12 20:44:37 +0300

    Opt in to ExperimentalLayoutApi for GenreChipGroup

--- designSystem/src/main/java/com/london/designsystem/component/GenreChipGroup.kt
+++ designSystem/src/main/java/com/london/designsystem/component/GenreChipGroup.kt
@@ -1,12 +1,14 @@
 package com.london.designsystem.component
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun GenreChipGroup(
     genres: List<String>,
