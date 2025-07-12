8bdd74e89223f04847f94165dcaf10322a3f5f3a
abdo-essam <abdo.e.mosa@gmail.com>
2025-07-13 02:02:09 +0300

    feat(ui): Implement blur effect in ImageViewFilter
    Add blur functionality to the ImageViewFilter composable.
    This allows applying a blur effect to images displayed
    using this composable.

--- feature/ImageHaramBlur/src/main/java/com/ae/imageharamblur/ui/ImageViewFilter.kt
+++ feature/ImageHaramBlur/src/main/java/com/ae/imageharamblur/ui/ImageViewFilter.kt
@@ -1,12 +1,17 @@
 package com.ae.imageharamblur.ui
 
 import android.graphics.Bitmap
-import android.graphics.Color
 import android.os.Build
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.blur
 import androidx.compose.ui.graphics.asImageBitmap
