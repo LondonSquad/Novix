c9c8969e56d7294f93f57ee90f52ada529481585
Bassant Mohammed <bassant.22230@gmail.com>
2025-07-12 18:23:49 +0300

    refactor: add clip to outline button modifier

--- designSystem/src/main/java/com/london/designsystem/component/button/OutlineButton.kt
+++ designSystem/src/main/java/com/london/designsystem/component/button/OutlineButton.kt
@@ -15,6 +15,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.london.designsystem.R
@@ -33,18 +34,17 @@ fun OutlineButton(
     modifier: Modifier = Modifier
 ) {
     Button(
-        onClick = onClick,
         modifier = modifier
             .height(48.dp)
             .defaultMinSize(minWidth = 79.dp)
+            .clip(RoundedCornerShape(12.dp))
             .border(1.dp, color = NovixTheme.colors.stroke),
-        shape = RoundedCornerShape(12.dp),
+        onClick = onClick,
         colors = ButtonDefaults.buttonColors(
             containerColor = NovixTheme.colors.surface,
             contentColor = if (isDisabled) NovixTheme.colors.disable else NovixTheme.colors.primary
         ),
         contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
-        elevation = ButtonDefaults.buttonElevation(defaultElevation = 2.dp)
     ) {
         if (hasLabel && text != null) {
             Text(
