73f42dad9a46ea4c9f2e5f4f5c923b857e11d243
Abdulrahman1890 <Abdulrahmanmm2011@hotmail.com>
2025-07-14 21:46:04 +0300

    feat: add Top Picks Details screen ui

--- presentation/src/main/java/com/london/presentation/screen/details/actor/TopPicksDetails.kt
+++ presentation/src/main/java/com/london/presentation/screen/details/actor/TopPicksDetails.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.london.designsystem.component.HomeCard
 import com.london.designsystem.component.TopBar
