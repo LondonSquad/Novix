1eb240d685681f739bcbcc650976d0ad453e913a
Karim Al-Zmzmy <karam.zmzmy@gmail.com>
2025-07-16 02:17:36 +0300

    chore: re-enables the Firebase Performance Monitoring, Google Services, and Firebase Crashlytics

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,9 +6,9 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.ksp)
-    //alias(libs.plugins.google.firebase.firebase.perf)
-    //alias(libs.plugins.google.gms.google.services)
-    //alias(libs.plugins.google.firebase.crashlytics)
+    alias(libs.plugins.google.firebase.firebase.perf)
+    alias(libs.plugins.google.gms.google.services)
+    alias(libs.plugins.google.firebase.crashlytics)
     alias(libs.plugins.kotlinx.kover)
 }
 
