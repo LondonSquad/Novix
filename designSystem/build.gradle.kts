9fd6448e1ad901ae9e9fa572e1d0a0722e426f66
abdo-essam <abdo.e.mosa@gmail.com>
2025-07-15 15:54:58 +0300

    feat(designSystem): Remove Lottie dependency
    Removes the Lottie animation dependency from the designSystem
    module. This dependency is no longer needed in the module.

--- designSystem/build.gradle.kts
+++ designSystem/build.gradle.kts
@@ -47,7 +47,4 @@ dependencies {
     debugImplementation(libs.bundles.compose.debug)
     androidTestImplementation(libs.bundles.base.testing)
     testImplementation(libs.bundles.testing)
-
-    // lottie animation
-    implementation(libs.lottie.compose)
 }
