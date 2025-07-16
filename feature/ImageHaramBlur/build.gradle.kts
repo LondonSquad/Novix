3adb8999114e85b78423ffd56557a2b7aad0bc42
abdo-essam <abdo.e.mosa@gmail.com>
2025-07-16 14:41:15 +0300

    refactor: Disable additional lint check for Flow operator in composition

--- feature/ImageHaramBlur/build.gradle.kts
+++ feature/ImageHaramBlur/build.gradle.kts
@@ -40,7 +40,7 @@ android {
     lint {
         disable += "CoroutineCreationDuringComposition"
         disable += "StateFlowValueCalledInComposition"
-
+        disable += "FlowOperatorInvokedInComposition"
     }
 
 
