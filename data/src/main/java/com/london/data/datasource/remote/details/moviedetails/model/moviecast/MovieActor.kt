369221f58407ba81c97bd306fa8f6c0133244282
abdelrhman1002 <abdelrhmanshabana999@gmail.com>
2025-07-16 04:37:58 +0300

    add: Create MovieActor data class

---
+++ data/src/main/java/com/london/data/datasource/remote/details/moviedetails/model/moviecast/MovieActor.kt
@@ -0,0 +1,26 @@
+package com.london.data.datasource.remote.details.moviedetails.model.moviecast
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class MovieActor(
+    val adult: Boolean,
+    val gender: Int,
+    @SerialName("id")
+    val id: Int,
+    @SerialName("known_for_department")
+    val knownForDepartment: String,
+    val name: String,
+    @SerialName("original_name")
+    val originalName: String,
+    val popularity: Double,
+    @SerialName("profile_path")
+    val profilePath: String? = null,
+    @SerialName("cast_id")
+    val castId: Int,
+    val character: String,
+    @SerialName("credit_id")
+    val creditId: String,
+    val order: Int
+)
