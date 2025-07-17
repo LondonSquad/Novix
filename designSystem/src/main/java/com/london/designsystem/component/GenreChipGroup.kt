320dd6f537d03a54568c66013cd061e2bbf07f7a
Bassant Mohammed <102248144+BassantM@users.noreply.github.com>
2025-07-17 07:09:42 +0300

    hotfix: solve bottom sheet crash (#130)
    * hotfix: solve bottom sheet crash
    
    * hotfix: genre chip group in release mode and filter bottom sheet
    
    ---------
    
    Co-authored-by: london-bot[bot] <220529784+london-bot[bot]@users.noreply.github.com>

--- designSystem/src/main/java/com/london/designsystem/component/GenreChipGroup.kt
+++ designSystem/src/main/java/com/london/designsystem/component/GenreChipGroup.kt
@@ -1,28 +1,31 @@
 package com.london.designsystem.component
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 
-@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun GenreChipGroup(
     availableGenres: List<Pair<Int, Int>>,
     selectedGenres: List<Int>,
     onGenreSelectionChanged: (List<Int>) -> Unit
 ) {
-    FlowRow(
+    LazyHorizontalGrid(
+        rows = GridCells.Fixed(2),
         modifier = Modifier
+            .height(80.dp)
             .fillMaxWidth(),
         horizontalArrangement = Arrangement.spacedBy(8.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        availableGenres.forEach { (genreId, genreName) ->
+        items(availableGenres) { (genreId, genreName) ->
             NovixChip(
                 text = stringResource(genreName),
                 isSelected = selectedGenres.contains(genreId),
