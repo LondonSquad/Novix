28409d6001dea2017be9fa109a6e51ad98ad04b5
abdelrhman1002 <abdelrhmanshabana999@gmail.com>
2025-07-17 03:58:57 +0300

    refactor: simplify remote data source methods

--- data/src/main/java/com/london/data/datasource/remote/details/actordetails/model/actormoviedetails/ActorMovieDetailsResponse.kt
+++ data/src/main/java/com/london/data/datasource/remote/details/actordetails/model/actormoviedetails/ActorMovieDetailsResponse.kt
@@ -4,9 +4,9 @@ import kotlinx.serialization.SerialName
 
 data class ActorMovieDetailsResponse(
     @SerialName("cast")
-    val cast: List<ActorMovieCastMember>,
+    val cast: List<ActorMovieCastMember>?,
     @SerialName("crew")
-    val crew: List<MovieCrewMember>,
+    val crew: List<MovieCrewMember>?,
     @SerialName("id")
-    val id: Int
+    val id: Int?
 )
