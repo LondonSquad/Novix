d8d3eccf1679e7aeae41b7b2d47f3a24a4477845
Marwan Qashwa <marawankashwa24@gmail.com>
2025-07-17 07:07:39 +0300

    fix: solve koin scan problem

--- app/src/main/java/com/london/app/di/AppModule.kt
+++ app/src/main/java/com/london/app/di/AppModule.kt
@@ -3,10 +3,12 @@ package com.london.app.di
 import android.content.Context
 import com.london.data.datasource.device.DeviceConfigurationDataSource
 import com.london.data.di.DataModule
+import org.koin.core.annotation.ComponentScan
 import org.koin.core.annotation.Module
 import org.koin.core.annotation.Single
 
 @Module(includes = [DataModule::class])
+@ComponentScan("com.london")
 class AppModule {
     @Single
     fun provideDeviceConfigurationDataSource(context: Context): DeviceConfigurationDataSource {
