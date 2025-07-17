efec2774ae3d0c0fb4101321b708a7227a48aa60
Bassant Mohammed <bassant.22230@gmail.com>
2025-07-17 19:20:44 +0300

    fix: update Actor DTOs for null safety

--- data/src/main/java/com/london/data/datasource/remote/details/actordetails/model/ActorDetailsResponse.kt
+++ data/src/main/java/com/london/data/datasource/remote/details/actordetails/model/ActorDetailsResponse.kt
@@ -7,43 +7,30 @@ import kotlinx.serialization.Serializable
 data class ActorDetailsResponse(
     @SerialName("adult")
     val adult: Boolean,
-
     @SerialName("also_known_as")
     val alsoKnownAs: List<String>,
-
     @SerialName("biography")
-    val biography: String,
-
+    val biography: String?,
     @SerialName("birthday")
-    val birthday: String,
-
+    val birthday: String?,
     @SerialName("deathday")
     val deathDay: String? = null,
-
     @SerialName("gender")
-    val gender: Int,
-
+    val gender: Int?,
     @SerialName("homepage")
     val homePage: String? = null,
-
     @SerialName("id")
-    val id: Int,
-
+    val id: Int?,
     @SerialName("imdb_id")
-    val imdbId: String,
-
+    val imdbId: String?,
     @SerialName("known_for_department")
-    val knownForDepartment: String,
-
+    val knownForDepartment: String?,
     @SerialName("name")
-    val name: String,
-
+    val name: String?,
     @SerialName("place_of_birth")
-    val placeOfBirth: String,
-
+    val placeOfBirth: String?,
     @SerialName("popularity")
-    val popularity: Double,
-
+    val popularity: Double?,
     @SerialName("profile_path")
-    val profilePath: String
+    val profilePath: String?
 )
