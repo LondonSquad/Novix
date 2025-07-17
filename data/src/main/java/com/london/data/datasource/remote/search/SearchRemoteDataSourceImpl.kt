f664853937939ffd5a13826e540b195f94d1787e
ahmbd <ahmbdelkhalek@gmail.com>
2025-07-17 15:11:30 +0300

    Refactor: Update Ktor client configuration and add Koin annotations

--- data/src/main/java/com/london/data/datasource/remote/search/SearchRemoteDataSourceImpl.kt
+++ data/src/main/java/com/london/data/datasource/remote/search/SearchRemoteDataSourceImpl.kt
@@ -7,7 +7,9 @@ import com.london.data.datasource.remote.search.model.SearchMovieRemote
 import com.london.data.datasource.remote.search.model.SearchTvShowRemote
 import com.london.data.utils.get
 import io.ktor.client.HttpClient
+import org.koin.core.annotation.Single
 
+@Single
 class SearchRemoteDataSourceImpl(
     private val ktorClient: HttpClient
 ) : SearchRemoteDataSource {
