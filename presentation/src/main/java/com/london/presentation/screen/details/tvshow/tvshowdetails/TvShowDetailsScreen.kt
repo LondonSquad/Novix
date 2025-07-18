e550334acb15bf8285cee4eeb50a120d31d8d1e5
Karim Al-Zmzmy <karam.zmzmy@gmail.com>
2025-07-18 21:08:20 +0300

    feat: add date convert function for tv show details

--- presentation/src/main/java/com/london/presentation/screen/details/tvshow/tvshowdetails/TvShowDetailsScreen.kt
+++ presentation/src/main/java/com/london/presentation/screen/details/tvshow/tvshowdetails/TvShowDetailsScreen.kt
@@ -69,6 +69,7 @@ import com.london.domain.entity.tvshowdetails.ImageItemEntity
 import com.london.domain.entity.tvshowdetails.TvShowCastMemberEntity
 import com.london.presentation.composables.ConditionalText
 import com.london.presentation.utils.Listen
+import com.london.presentation.utils.convertDate
 import com.london.presentation.utils.toLocalizedNumbers
 import kotlinx.coroutines.delay
 import org.koin.androidx.compose.koinViewModel
