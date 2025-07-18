b45b6f5c5e4ccd2cfa0753f02972fa2d1bd65f77
AsmaaKhaledGomaa <asmaa.kh.gomaa@gmail.com>
2025-07-18 15:14:51 +0300

    refactor: Remove unused clickable modifier from TopBar

--- designSystem/src/main/java/com/london/designsystem/component/TopBar.kt
+++ designSystem/src/main/java/com/london/designsystem/component/TopBar.kt
@@ -2,7 +2,6 @@ package com.london.designsystem.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
