0aef4ad555e97c40e5b1e1d37b9dc9462ef57109
Bassant Mohammed <102248144+BassantM@users.noreply.github.com>
2025-07-19 03:10:48 +0300

    fix: replace width with fillMaxWidth in ActorItem to enhance tv show cast section (#211)

--- designSystem/src/main/java/com/london/designsystem/component/ActorItem.kt
+++ designSystem/src/main/java/com/london/designsystem/component/ActorItem.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -40,7 +39,7 @@ fun ActorItem(
     modifier: Modifier = Modifier
 ) {
     Row(
-        modifier = modifier.width(296.dp),
+        modifier = modifier.fillMaxWidth(),
         verticalAlignment = Alignment.Bottom,
     ) {
         ActorImage(imageRes = imageRes)
