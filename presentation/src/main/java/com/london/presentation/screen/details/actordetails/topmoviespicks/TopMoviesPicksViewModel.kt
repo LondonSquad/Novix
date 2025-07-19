4ee5e0b4cd8f672860d265dc19e665a2b02fbb9e
ahmbd <ahmbdelkhalek@gmail.com>
2025-07-19 21:18:53 +0300

    Refactor: Update actor gallery and top movies picks screens with BaseViewModel

--- presentation/src/main/java/com/london/presentation/screen/details/actordetails/topmoviespicks/TopMoviesPicksViewModel.kt
+++ presentation/src/main/java/com/london/presentation/screen/details/actordetails/topmoviespicks/TopMoviesPicksViewModel.kt
@@ -1,54 +1,57 @@
 package com.london.presentation.screen.details.actordetails.topmoviespicks
 
-import android.util.Log
 import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.ViewModel
 import com.london.domain.usecase.GetActorMoviePicksByIdUseCase
-import com.london.presentation.navigation.arguments.TopMoviesArgs
-import com.london.presentation.utils.launchCatching
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
+import com.london.presentation.features.base.BaseViewModel
+import com.london.presentation.navigation.Screen
+import com.london.presentation.navigation.getArgs
 import org.koin.android.annotation.KoinViewModel
 
 @KoinViewModel
 class TopMoviesPicksViewModel(
     private val getActorMoviePicksById: GetActorMoviePicksByIdUseCase,
     savedStateHandle: SavedStateHandle,
-) : ViewModel(), TopMoviesPicksInteractions {
+) : BaseViewModel<TopMoviesPicksUiState, TopMoviesPicksEffectUiState>(TopMoviesPicksUiState()),
+    TopMoviesPicksContract {
 
-    private val _uiState = MutableStateFlow(TopMoviesPicksUiState())
-    val uiState: StateFlow<TopMoviesPicksUiState> = _uiState.asStateFlow()
-
-    private val args by lazy { TopMoviesArgs(savedStateHandle) }
+    private val args =  savedStateHandle.getArgs<Screen.ActorTopMoviesPicksDetails>()
+    val actorId = args?.actorId?: 0
 
     init {
-        if (args.actorId != 0) {
+        if (actorId != 0) {
             getActorMoviePicksData()
         }
     }
 
     private fun getActorMoviePicksData() {
-        launchCatching {
-            try {
-                _uiState.update {
-                    it.copy(
-                        id = it.id,
-                        movieDetails = getActorMoviePicksById.invoke(args.actorId),
-                        isSaved = it.isSaved,
-                        backdropPath = it.backdropPath,
+
+        tryToExecute(
+            block = { getActorMoviePicksById.invoke(actorId) },
+            onStart = { updateState { copy(isLoading = true) } },
+            onSuccess = { actorMovieDetails ->
+                updateState {
+                    copy(
+                        id = actorMovieDetails.id,
+                        movieDetails = actorMovieDetails,
+                        isSaved = isSaved,
+                        backdropPath = backdropPath,
                     )
                 }
-            } catch (e: Exception){
-                Log.d("TAG", "getActorMoviePicksData: $e")
-            }
-        }
+            },
+            onError = { errorState ->
+                updateState { copy(errorState = errorState) }
+            },
+            onCompleted = {updateState { copy(isLoading = true) }},
+            checkSuccess = {actorId != 0},
+        )
     }
 
     override fun onSaveMovie(movieId: Int) {
-        _uiState.update {
-            it.copy(isSaved = !it.isSaved)
-        }
+        updateState { copy(isSaved = isSaved) }
+        emitEffect(TopMoviesPicksEffectUiState.NavigationToMovieDetails(movieId))
+    }
+
+    override fun onClickBack() {
+        emitEffect(TopMoviesPicksEffectUiState.NavigateBack)
     }
 }
