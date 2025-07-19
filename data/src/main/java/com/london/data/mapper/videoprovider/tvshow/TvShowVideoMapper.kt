129827c128d8904cee895119507015970f67b710
abdelrhman1002 <abdelrhmanshabana999@gmail.com>
2025-07-19 05:43:08 +0300

    Add TV show video fetching functionality (#230)
    Co-authored-by: Ahmed Mones <68463985+Al-Taie@users.noreply.github.com>

---
+++ data/src/main/java/com/london/data/mapper/videoprovider/tvshow/TvShowVideoMapper.kt
@@ -0,0 +1,37 @@
+package com.london.data.mapper.videoprovider.tvshow
+
+import com.london.data.datasource.remote.details.videoprovider.tvshow.TvShowVideoRemote
+import com.london.data.utils.asYoutubeUrlOrEmpty
+import com.london.data.utils.isTrue
+import com.london.data.utils.orZero
+import com.london.domain.entity.videoprovider.TvShowVideo
+
+fun TvShowVideoRemote.toTvShowVideo(): TvShowVideo {
+    return TvShowVideo(
+        id = id.orEmpty(),
+        iso31661 = iso31661.orEmpty(),
+        iso6391 = iso31661.orEmpty(),
+        videoUrl = key.asYoutubeUrlOrEmpty(),
+        name = name.orEmpty(),
+        official = official.isTrue,
+        publishedAt = publishedAt.orEmpty(),
+        site = site.orEmpty(),
+        size = size.orZero(),
+        type =type.orEmpty()
+    )
+}
+
+fun TvShowVideo.TvShowVideoRemote(): TvShowVideoRemote {
+    return TvShowVideoRemote(
+        id = id,
+        iso31661 = iso6391,
+        iso6391 = iso6391,
+        key = videoUrl,
+        name = name,
+        official = official,
+        publishedAt = publishedAt,
+        site = site,
+        size = size,
+        type = type
+    )
+}
