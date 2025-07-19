2e9fc238032f6cae1dca8794bb235e25c12879a7
Abdulrahman1890 <Abdulrahmanmm2011@hotmail.com>
2025-07-19 03:39:14 +0300

    fix: top bar alignment

--- designSystem/src/main/java/com/london/designsystem/component/TopBar.kt
+++ designSystem/src/main/java/com/london/designsystem/component/TopBar.kt
@@ -6,13 +6,11 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -35,7 +33,7 @@ fun TopBar(
     Row(
         modifier = modifier
             .fillMaxWidth()
-            .padding(vertical = 13.dp),
+            .padding(vertical = 8.dp),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
@@ -47,13 +45,12 @@ fun TopBar(
                 text = title,
                 style = NovixTheme.typography.title.large,
                 color = NovixTheme.colors.title,
+                maxLines = 1,
                 modifier = Modifier
                     .weight(1f)
                     .padding(horizontal = 12.dp)
             )
         }
-
-        Spacer(modifier = Modifier.weight(1f))
 
         Row(
             horizontalArrangement = Arrangement.spacedBy(8.dp),
