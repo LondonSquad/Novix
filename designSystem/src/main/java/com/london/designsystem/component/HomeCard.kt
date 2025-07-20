3f22b072edd3adf300d54345634f7ed9877ce3b1
ahmbd <ahmbdelkhalek@gmail.com>
2025-07-21 00:39:51 +0300

    feat: make HomeCard save icon optional

--- designSystem/src/main/java/com/london/designsystem/component/HomeCard.kt
+++ designSystem/src/main/java/com/london/designsystem/component/HomeCard.kt
@@ -1,7 +1,6 @@
 package com.london.designsystem.component
 
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -23,8 +22,8 @@ fun HomeCard(
     imageUrl: Any,
     modifier: Modifier = Modifier,
     isSaved: Boolean = false,
+    hasSaveIcon: Boolean = true,
     imageDescription: String? = null,
-    onCardClick: () -> Unit = {},
     onSaveClick: () -> Unit,
 ) {
 
@@ -32,7 +31,6 @@ fun HomeCard(
         modifier = modifier
             .fillMaxWidth()
             .aspectRatio(158f / 210f)
-            .clickable { onCardClick() }
             .clip(RoundedCornerShape(12.dp))
             .border(
                 width = 1.dp,
@@ -51,15 +49,15 @@ fun HomeCard(
             loadingContent = { CircularLoading(modifier = Modifier.align(Alignment.Center)) },
             moderatedContent = { UnSuitableEye() }
         )
-        SaveIcon(
-            isSaved = isSaved,
-            onSaveClick = onSaveClick,
-            modifier = Modifier
-                .padding(8.dp)
-                .align(Alignment.TopStart),
-            iconTint = NovixTheme.colors.onPrimary,
-
-        )
+        if (hasSaveIcon)
+            SaveIcon(
+                isSaved = isSaved,
+                onSaveClick = onSaveClick,
+                modifier = Modifier
+                    .padding(8.dp)
+                    .align(Alignment.TopStart),
+                iconTint = NovixTheme.colors.onPrimary,
+            )
     }
 }
 
--- presentation/src/main/java/com/london/presentation/screen/home/PopularSection.kt
+++ presentation/src/main/java/com/london/presentation/screen/home/PopularSection.kt
@@ -1,5 +1,6 @@
 package com.london.presentation.screen.home
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -45,8 +46,8 @@ fun PopularSection(
     modifier: Modifier = Modifier,
     pagerState: PagerState,
     images: List<String>,
-    onSaveClick: () -> Unit = {},
-    onCardClick: () -> Unit = {},
+    onSaveClick: () -> Unit,
+    onCardClick: () -> Unit,
 ) {
     val density = LocalDensity.current
     val screenWidth = with(density) {
@@ -91,10 +92,10 @@ fun PopularSection(
                         )
 
                         transformOrigin = TransformOrigin(TRANSFORM_ORIGIN_X, TRANSFORM_ORIGIN_Y)
-                    },
-                onCardClick = onCardClick,
+                    }.clickable { onCardClick() },
                 imageUrl = images[page],
                 onSaveClick = { onSaveClick() },
+                hasSaveIcon = pagerState.currentPage == page
             )
         }
 
