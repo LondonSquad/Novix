605275ddd2a01b87c185b13ac3fe8740a251af68
Aseel AbuKmail <aseelabukmail@gmail.com>
2025-07-21 01:26:21 +0300

    Refactor: Remove language parameter from search use cases and repository

--- data/src/main/java/com/london/data/repository/SearchRepositoryImpl.kt
+++ data/src/main/java/com/london/data/repository/SearchRepositoryImpl.kt
@@ -46,18 +46,18 @@ class SearchRepositoryImpl(
         }.onFailure { crashReporter?.logException(it) }.getOrThrow()
 
     override suspend fun searchForMovies(
-        name: String, language: String, pageNumber: Int
+        name: String, pageNumber: Int
     ): PagedFetchResponse<Movie> = fetchAndSync(
         cacheBlock = {
         localMovieDataSource.getByQueryAndPage(
-            query = name + language, page = pageNumber
+            query = name, page = pageNumber
         )
     }, networkBlock = {
         remoteDataSource.searchForMovies(
             query = name,
             includeAdult = false,
             pageNumber = pageNumber,
-        ).toLocal(query = name + language)
+        ).toLocal(query = name)
     }, syncBlock = { localMovieDataSource.insert(it) }, crashReporter = crashReporter
     ).run {
         PagedFetchResponse(
@@ -69,18 +69,18 @@ class SearchRepositoryImpl(
     }
 
     override suspend fun searchForTvShows(
-        name: String, language: String, pageNumber: Int
+        name: String, pageNumber: Int
     ): PagedFetchResponse<TvShow> = fetchAndSync(
         cacheBlock = {
         localTvShowDataSource.getByQueryAndPage(
-            query = name + language, page = pageNumber
+            query = name, page = pageNumber
         )
     }, networkBlock = {
         remoteDataSource.searchForTvShows(
             query = name,
             includeAdult = false,
             pageNumber = pageNumber,
-        ).toLocal(query = name + language)
+        ).toLocal(query = name)
     }, syncBlock = { localTvShowDataSource.insert(it) }, crashReporter = crashReporter
     ).run {
         PagedFetchResponse(
@@ -92,18 +92,18 @@ class SearchRepositoryImpl(
     }
 
     override suspend fun searchForActors(
-        name: String, language: String, pageNumber: Int
+        name: String, pageNumber: Int
     ): PagedFetchResponse<Actor> = fetchAndSync(
         cacheBlock = {
         localActorDataSource.getByQueryAndPage(
-            query = name + language, page = pageNumber
+            query = name, page = pageNumber
         )
     }, networkBlock = {
         remoteDataSource.searchForActors(
             query = name,
             includeAdult = false,
             pageNumber = pageNumber,
-        ).toLocal(query = name + language)
+        ).toLocal(query = name)
     }, syncBlock = { localActorDataSource.insert(it) }, crashReporter = crashReporter
     ).run {
         PagedFetchResponse(
@@ -115,7 +115,7 @@ class SearchRepositoryImpl(
     }
 
     override suspend fun searchForMoviesByCategory(
-        categoryId: Int, language: String, pageNumber: Int
+        categoryId: Int, pageNumber: Int
     ): PagedFetchResponse<Movie> = fetchAndSync(
         networkBlock = {
             remoteDataSource.getMoviesByCategory(
