9a79abd9fb116c3629dd96809b61646114b3eded
ahmbd <ahmbdelkhalek@gmail.com>
2025-07-21 16:04:14 +0300

    feat: implement safe API call utility

--- data/src/main/java/com/london/data/datasource/remote/home/popular/PopularRemoteDataSourceImpl.kt
+++ data/src/main/java/com/london/data/datasource/remote/home/popular/PopularRemoteDataSourceImpl.kt
@@ -3,11 +3,12 @@ package com.london.data.datasource.remote.home.popular
 import com.london.data.datasource.remote.ApiResponse
 import com.london.data.datasource.remote.home.popular.api.PopularApiService
 import com.london.data.datasource.remote.home.popular.model.PopularMovieResponse
+import com.london.data.utils.safeCallApi
 
 class PopularRemoteDataSourceImpl(
     private val popularApiService: PopularApiService
 ) : PopularRemoteDataSource {
     override suspend fun getPopularMovies(): ApiResponse<PopularMovieResponse> {
-        return popularApiService.getPopularMovies()
+        return safeCallApi { popularApiService.getPopularMovies() }
     }
 }
