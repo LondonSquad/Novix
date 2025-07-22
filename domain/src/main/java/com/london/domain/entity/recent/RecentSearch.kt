70f821f7b7e08ac2aed8779d06bc2bdcf5f9f7af
Marwan Qashwa <marawankashwa24@gmail.com>
2025-07-22 22:23:05 +0300

    Revert "style: Ignore seedwork files from Kover report"
    This reverts commit 47c19576428b525396e5cb96f523f40dab6a29a6.

--- domain/src/main/java/com/london/domain/entity/recent/RecentSearch.kt
+++ domain/src/main/java/com/london/domain/entity/recent/RecentSearch.kt
@@ -1,8 +1,5 @@
 package com.london.domain.entity.recent
 
-import com.london.domain.KoverIgnore
-
-@KoverIgnore
 data class RecentSearch(
     val id: Int,
     val query: String,
--- domain/src/main/java/com/london/domain/entity/review/ReviewEntity.kt
+++ domain/src/main/java/com/london/domain/entity/review/ReviewEntity.kt
@@ -1,8 +1,5 @@
 package com.london.domain.entity.review
 
-import com.london.domain.KoverIgnore
-
-@KoverIgnore
 data class ReviewEntity(
     val authorName: String,
     val authorDetails: AuthorDetails,
@@ -13,7 +10,6 @@ data class ReviewEntity(
     val url: String
 )
 
-@KoverIgnore
 data class AuthorDetails(
     val name: String,
     val username: String,
