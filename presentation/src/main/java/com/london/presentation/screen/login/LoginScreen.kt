31a2909261e9d8892feeeee9aa4f395c392e1cfc
Mostafa Mohammed <tofymofy.55@gmail.com>
2025-07-22 22:47:07 +0300

    Fix: Adjust LoginScreen layout for navigation bar

--- presentation/src/main/java/com/london/presentation/screen/login/LoginScreen.kt
+++ presentation/src/main/java/com/london/presentation/screen/login/LoginScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -161,7 +162,9 @@ private fun Content(
             )
             Spacer(modifier = Modifier.weight(1f))
             Row(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(WindowInsets.navigationBars.asPaddingValues()),
                 horizontalArrangement = Arrangement.Center,
                 verticalAlignment = Alignment.CenterVertically
             ) {
