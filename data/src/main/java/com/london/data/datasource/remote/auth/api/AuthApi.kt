c75329f7922f0a2cf5787ed6dffe0c381dd57b86
Karim Al-Zmzmy <karam.zmzmy@gmail.com>
2025-07-22 07:56:42 +0300

    feat: add AuthApi methods and update paths

--- data/src/main/java/com/london/data/datasource/remote/auth/api/AuthApi.kt
+++ data/src/main/java/com/london/data/datasource/remote/auth/api/AuthApi.kt
@@ -1,28 +1,33 @@
 package com.london.data.datasource.remote.auth.api
 
-import com.london.data.datasource.remote.auth.model.CreateSessionWithLoginRequest
 import com.london.data.datasource.remote.auth.model.DeleteSessionResponse
 import com.london.data.datasource.remote.auth.model.GuestSessionResponse
 import com.london.data.datasource.remote.auth.model.RequestTokenResponse
 import com.london.data.datasource.remote.auth.model.SessionResponse
+import com.london.data.datasource.remote.auth.model.Token
+import com.london.data.datasource.remote.auth.model.ValidateKeyResponse
+import com.london.data.datasource.remote.auth.model.ValidateWithLoginRequestBody
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface AuthApi {
-    @GET("authentication/token/new")
-    suspend fun getRequestToken(): RequestTokenResponse
-
-    @POST("authentication/token/validate_with_login")
-    suspend fun validateWithLogin(@Body request: CreateSessionWithLoginRequest): Boolean
-
-    @POST("authentication/session/new")
-    suspend fun createSession(@Body token: RequestTokenResponse): SessionResponse
-
-    @GET("authentication/guest_session/new")
+    @GET("3/authentication/guest_session/new")
     suspend fun createGuestSession(): GuestSessionResponse
 
-    @DELETE("authentication/session")
+    @GET("3/authentication/token/new")
+    suspend fun createRequestToken(): RequestTokenResponse
+
+    @POST("3/authentication/session/new")
+    suspend fun createSession(@Body requestBody: Token): SessionResponse
+
+    @POST("3/authentication/token/validate_with_login")
+    suspend fun createSessionWithLogin(@Body requestBody: ValidateWithLoginRequestBody): RequestTokenResponse
+
+    @DELETE("3/authentication/session")
     suspend fun deleteSession(): DeleteSessionResponse
+
+    @GET("3/authentication")
+    suspend fun validateKey(): ValidateKeyResponse
 }
