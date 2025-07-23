16875ec46b35258bc48e60bf284de467a1ca7e0e
ahmbd <ahmbdelkhalek@gmail.com>
2025-07-23 18:49:37 +0300

    refactor: Make fields in PopularTvShowResponse nullable

--- data/src/main/java/com/london/data/datasource/remote/home/popular/model/PopularTvShowResponse.kt
+++ data/src/main/java/com/london/data/datasource/remote/home/popular/model/PopularTvShowResponse.kt
@@ -6,31 +6,31 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class PopularTvShowResponse(
     @SerialName("adult")
-    val adult: Boolean,
+    val adult: Boolean?,
     @SerialName("backdrop_path")
     val backdropPath: String?,
     @SerialName("genre_ids")
-    val genreIds: List<Int>,
+    val genreIds: List<Int>?,
     @SerialName("id")
-    val id: Int,
+    val id: Int?,
     @SerialName("origin_country")
-    val originCountry: List<String>,
+    val originCountry: List<String>?,
     @SerialName("original_language")
-    val originalLanguage: String,
+    val originalLanguage: String?,
     @SerialName("original_name")
-    val originalName: String,
+    val originalName: String?,
     @SerialName("overview")
-    val overview: String,
+    val overview: String?,
     @SerialName("popularity")
-    val popularity: Double,
+    val popularity: Double?,
     @SerialName("poster_path")
     val posterPath: String?,
     @SerialName("first_air_date")
-    val firstAirDate: String,
+    val firstAirDate: String?,
     @SerialName("name")
-    val name: String,
+    val name: String?,
     @SerialName("vote_average")
-    val voteAverage: Double,
+    val voteAverage: Double?,
     @SerialName("vote_count")
-    val voteCount: Int
+    val voteCount: Int?
 )
--- data/src/main/java/com/london/data/mapper/popular/PopularTvShowMapper.kt
+++ data/src/main/java/com/london/data/mapper/popular/PopularTvShowMapper.kt
@@ -1,4 +1,5 @@
 @file:KoverIgnore
+
 package com.london.data.mapper.popular
 
 import com.london.data.datasource.remote.ApiResponse
@@ -8,16 +9,15 @@ import com.london.data.utils.orZero
 import com.london.domain.KoverIgnore
 import com.london.domain.entity.popular.PopularTvShow
 
-fun PopularTvShowResponse.toPopularTvShow(): PopularTvShow{
+fun PopularTvShowResponse.toPopularTvShow(): PopularTvShow {
     return PopularTvShow(
         id = id.orZero(),
-        name = name,
-        overview = overview,
+        name = name.orEmpty(),
+        overview = overview.orEmpty(),
         posterUrl = posterPath.asImageUrlOrEmpty(),
-        rating = voteAverage
+        rating = voteAverage.orZero()
     )
 }
 
-fun ApiResponse<PopularTvShowResponse>.toPopularTvShows(): List<PopularTvShow> {
-    return items.map { it.toPopularTvShow() }
-}
+fun ApiResponse<PopularTvShowResponse>.toPopularTvShows(): List<PopularTvShow> =
+    items.map { it.toPopularTvShow() }
