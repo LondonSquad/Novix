94c5905f5893500e9a30503514b672529451c045
AsmaaKhaledGomaa <asmaa.kh.gomaa@gmail.com>
2025-07-24 17:59:10 +0300

    feat: Add Trending TV Shows endpoint

--- data/src/main/java/com/london/data/datasource/remote/home/trending/TrendingRemoteDataSource.kt
+++ data/src/main/java/com/london/data/datasource/remote/home/trending/TrendingRemoteDataSource.kt
@@ -1,8 +1,9 @@
 package com.london.data.datasource.remote.home.trending
 
-import com.london.data.datasource.remote.ApiResponse
 import com.london.data.datasource.remote.home.trending.model.TrendingMovieResponse
+import com.london.data.datasource.remote.home.trending.model.TrendingTvShowListResponse
 
 interface TrendingRemoteDataSource {
     suspend fun getTrendingMovies(page: Int): TrendingMovieResponse
+    suspend fun getTrendingTvShows(page: Int): TrendingTvShowListResponse
 }
