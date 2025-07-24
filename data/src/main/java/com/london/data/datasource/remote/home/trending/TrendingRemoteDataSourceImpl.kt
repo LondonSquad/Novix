192b39181c94512d64feddfed62673eda5f2f5b4
AsmaaKhaledGomaa <asmaa.kh.gomaa@gmail.com>
2025-07-24 17:58:26 +0300

    feat: Add getTrendingTvShows to TrendingRemoteDataSource

--- data/src/main/java/com/london/data/datasource/remote/home/trending/TrendingRemoteDataSourceImpl.kt
+++ data/src/main/java/com/london/data/datasource/remote/home/trending/TrendingRemoteDataSourceImpl.kt
@@ -2,6 +2,7 @@ package com.london.data.datasource.remote.home.trending
 
 import com.london.data.datasource.remote.home.trending.api.TrendingApiService
 import com.london.data.datasource.remote.home.trending.model.TrendingMovieResponse
+import com.london.data.datasource.remote.home.trending.model.TrendingTvShowListResponse
 import org.koin.core.annotation.Single
 
 @Single
@@ -10,5 +11,9 @@ class TrendingRemoteDataSourceImpl(
 ) : TrendingRemoteDataSource {
     override suspend fun getTrendingMovies(page: Int): TrendingMovieResponse {
         return trendingApiService.getTrendingMovies(page)
+    }
+
+    override suspend fun getTrendingTvShows(page: Int): TrendingTvShowListResponse {
+        return trendingApiService.getTrendingTvShows(page)
     }
 } 
