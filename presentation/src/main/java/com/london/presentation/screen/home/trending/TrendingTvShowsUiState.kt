90934e6ccf961193918a6326bdfd719a1bbc9a16
AsmaaKhaledGomaa <asmaa.kh.gomaa@gmail.com>
2025-07-24 18:05:15 +0300

    feat: Add TrendingTvShowsUiState

---
+++ presentation/src/main/java/com/london/presentation/screen/home/trending/TrendingTvShowsUiState.kt
@@ -0,0 +1,16 @@
+package com.london.presentation.screen.home.trending
+
+import com.london.domain.entity.trending.TrendingTvShow
+import com.london.presentation.screen.base.ErrorState
+import com.london.presentation.utils.Genre
+import androidx.paging.PagingData
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+data class TrendingTvShowsUiState(
+    val trendingTvShows: Flow<PagingData<TrendingTvShow>> = flow {},
+    val errorState: ErrorState? = null,
+    val isLoading: Boolean = false,
+    val genres: List<Genre> = emptyList(),
+    val selectedGenreId: Int? = null
+) 
