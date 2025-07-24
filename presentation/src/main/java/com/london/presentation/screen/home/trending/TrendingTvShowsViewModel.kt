58c2a5eafd88fe83d699a3fcb2e1eea3d6310da0
AsmaaKhaledGomaa <asmaa.kh.gomaa@gmail.com>
2025-07-24 18:05:41 +0300

    feat: Implement TrendingTvShowsViewModel

---
+++ presentation/src/main/java/com/london/presentation/screen/home/trending/TrendingTvShowsViewModel.kt
@@ -0,0 +1,54 @@
+package com.london.presentation.screen.home.trending
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
+import com.london.domain.usecase.GetTrendingTvShowsUseCase
+import com.london.presentation.screen.base.createPagingSourceFlow
+import com.london.presentation.utils.Genre
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import org.koin.android.annotation.KoinViewModel
+
+@KoinViewModel
+class TrendingTvShowsViewModel(
+    private val getTrendingTvShows: GetTrendingTvShowsUseCase,
+) : ViewModel() {
+    private val _state = MutableStateFlow(TrendingTvShowsUiState())
+    val state: StateFlow<TrendingTvShowsUiState> = _state
+
+    private val _effect = MutableStateFlow<TrendingTvShowsEffect?>(null)
+    val effect: StateFlow<TrendingTvShowsEffect?> = _effect.asStateFlow()
+
+    init {
+        _state.value = _state.value.copy(
+            genres = Genre.entries.toList(),
+            selectedGenreId = Genre.All.id
+        )
+        fetchTrendingTvShows()
+    }
+
+    private fun fetchTrendingTvShows() {
+        val tvShowsFlow = createPagingSourceFlow("") { _, pageNumber ->
+            getTrendingTvShows.invoke(pageNumber)
+        }.cachedIn(viewModelScope)
+        _state.value = _state.value.copy(trendingTvShows = tvShowsFlow, isLoading = false)
+    }
+
+    fun onGenreSelected(genre: Genre) {
+        _state.value = _state.value.copy(selectedGenreId = genre.id)
+    }
+
+    fun onTvShowClick(tvShowId: Int) {
+        _effect.value = TrendingTvShowsEffect.NavigateToTvShow(tvShowId)
+    }
+
+    fun onBackClick() {
+        _effect.value = TrendingTvShowsEffect.NavigateBack
+    }
+
+    fun resetEffect() {
+        _effect.value = null
+    }
+} 
