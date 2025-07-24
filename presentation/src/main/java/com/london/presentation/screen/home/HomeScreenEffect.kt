dd4649be6bda0a8ee771ff35494f8d2b2fd71399
AsmaaKhaledGomaa <asmaa.kh.gomaa@gmail.com>
2025-07-24 15:20:38 +0300

    feat: Add NavigationTrendingCard to HomeScreenEffect

--- presentation/src/main/java/com/london/presentation/screen/home/HomeScreenEffect.kt
+++ presentation/src/main/java/com/london/presentation/screen/home/HomeScreenEffect.kt
@@ -2,4 +2,5 @@ package com.london.presentation.screen.home
 
 interface HomeScreenEffect {
     data class NavigationMovieDetails(val id: Int): HomeScreenEffect
+    data class NavigationTrendingCard(val id: Int): HomeScreenEffect
 }
