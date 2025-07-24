07ca9bb578bd62588b8079f4daf7d80086d4f11a
AsmaaKhaledGomaa <asmaa.kh.gomaa@gmail.com>
2025-07-24 17:57:13 +0300

    feat: Add Trending TV Shows API endpoint

--- data/src/main/java/com/london/data/datasource/remote/home/trending/api/TrendingApiService.kt
+++ data/src/main/java/com/london/data/datasource/remote/home/trending/api/TrendingApiService.kt
@@ -1,11 +1,14 @@
 package com.london.data.datasource.remote.home.trending.api
 
-import com.london.data.datasource.remote.ApiResponse
 import com.london.data.datasource.remote.home.trending.model.TrendingMovieResponse
+import com.london.data.datasource.remote.home.trending.model.TrendingTvShowListResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface TrendingApiService {
     @GET("3/trending/movie/day")
     suspend fun getTrendingMovies(@Query("page") page: Int): TrendingMovieResponse
+
+    @GET("3/trending/tv/day")
+    suspend fun getTrendingTvShows(@Query("page") page: Int): TrendingTvShowListResponse
 } 
