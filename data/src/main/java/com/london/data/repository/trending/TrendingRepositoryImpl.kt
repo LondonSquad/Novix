de434f8cda736c1ea5e6805e139ab09807070174
AsmaaKhaledGomaa <asmaa.kh.gomaa@gmail.com>
2025-07-24 15:49:24 +0300

    feat: Add TrendingTvShowResponse and provide TrendingApiService

---
+++ data/src/main/java/com/london/data/repository/trending/TrendingRepositoryImpl.kt
@@ -0,0 +1,24 @@
+package com.london.data.repository.trending
+
+import com.london.data.datasource.remote.home.trending.TrendingRemoteDataSource
+import com.london.data.mapper.trending.toTrendingMovie
+import com.london.domain.entity.trending.TrendingMovie
+import com.london.domain.entity.PagedFetchResponse
+import com.london.domain.repository.TrendingRepository
+import org.koin.core.annotation.Single
+
+@Single
+class TrendingRepositoryImpl(
+    private val trendingRemoteDataSource: TrendingRemoteDataSource
+) : TrendingRepository {
+    override suspend fun getTrendingMovies(page: Int): PagedFetchResponse<TrendingMovie> {
+        val response = trendingRemoteDataSource.getTrendingMovies(page)
+        val movies = response.results.map { it.toTrendingMovie() }
+        return PagedFetchResponse(
+            currentPage = response.page,
+            items = movies,
+            totalPages = response.totalPages,
+            totalItems = movies.size
+        )
+    }
+} 
