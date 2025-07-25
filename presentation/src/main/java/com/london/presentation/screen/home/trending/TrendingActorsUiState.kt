7741c199dde0fd3777c59fc41ebd9d042cf82fa8
AsmaaKhaledGomaa <asmaa.kh.gomaa@gmail.com>
2025-07-25 08:15:38 +0300

    feat: create TrendingActorsUiState

---
+++ presentation/src/main/java/com/london/presentation/screen/home/trending/TrendingActorsUiState.kt
@@ -0,0 +1,15 @@
+package com.london.presentation.screen.home.trending
+
+import com.london.domain.entity.Actor
+import com.london.presentation.screen.base.ErrorState
+import androidx.paging.PagingData
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+data class TrendingActorsUiState(
+    val actorsFlow: Flow<PagingData<Actor>> = flow {},
+    val errorState: ErrorState? = null,
+    val isLoading: Boolean = false,
+    val backdropPath: String = "",
+    val id: Int = 0
+) 
