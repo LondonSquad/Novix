b094adf6d54f9e70787371cc915e5426fc9be79a
Bassant Mohammed <bassant.22230@gmail.com>
2025-07-25 16:40:09 +0300

    fix: add serialization annotations in movie details

--- data/src/main/java/com/london/data/datasource/remote/details/moviedetails/model/moviedetails/MovieDetailsResponse.kt
+++ data/src/main/java/com/london/data/datasource/remote/details/moviedetails/model/moviedetails/MovieDetailsResponse.kt
@@ -4,15 +4,19 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class MovieDetailsResponse(
+    @SerialName("adult")
     val adult: Boolean?,
     @SerialName("backdrop_path")
     val backdropPath: String?,
     @SerialName("belongs_to_collection")
     val remoteBelongsToCollection: RemoteCollectionDetails?,
+    @SerialName("budget")
     val budget: Int?,
     @SerialName("genres")
     val genreRemote: List<GenreRemote>?,
+    @SerialName("homepage")
     val homepage: String?,
+    @SerialName("id")
     val id: Int?,
     @SerialName("imdb_id")
     val imdbId: String?,
@@ -22,7 +26,9 @@ data class MovieDetailsResponse(
     val originalLanguage: String?,
     @SerialName("original_title")
     val originalTitle: String?,
+    @SerialName("overview")
     val overview: String?,
+    @SerialName("popularity")
     val popularity: Double?,
     @SerialName("poster_path")
     val posterPath: String?,
@@ -32,13 +38,19 @@ data class MovieDetailsResponse(
     val productionCountries: List<ProductionCountryRemote>?,
     @SerialName("release_date")
     val releaseDate: String?,
+    @SerialName("revenue")
     val revenue: Long?,
+    @SerialName("runtime")
     val runtime: Int?,
     @SerialName("spoken_languages")
     val spokenLanguages: List<SpokenLanguageRemote>?,
+    @SerialName("status")
     val status: String?,
+    @SerialName("tagline")
     val tagline: String?,
+    @SerialName("title")
     val title: String?,
+    @SerialName("video")
     val video: Boolean?,
     @SerialName("vote_average")
     val voteAverage: Double?,
