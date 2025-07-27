41ebb73d02e9176d4764c3e73ba0a79533bae4d3
abdelrhman1002 <abdelrhmanshabana999@gmail.com>
2025-07-27 18:21:59 +0300

    refactor/top-rated-data-sources: integrate base datasource (#293)
    * refactor/top-rated-data-sources: integrate base datasource
    
    * refactor/top-rated-data-sources: fix data source usage

--- data/src/main/java/com/london/data/remote/source/toprated/tvseries/TopRatedTvRemoteDataSourceImpl.kt
+++ data/src/main/java/com/london/data/remote/source/toprated/tvseries/TopRatedTvRemoteDataSourceImpl.kt
@@ -5,15 +5,19 @@ package com.london.data.remote.source.toprated.tvseries
 import com.london.data.remote.model.ApiResponse
 import com.london.data.remote.model.toprated.tvshow.model.TopRatedTvSeriesRemote
 import com.london.data.remote.service.toprated.tvseries.TopRatedTvSeriesApiService
+import com.london.data.remote.source.base.BaseRemoteDatasource
 import com.london.domain.KoverIgnore
 import org.koin.core.annotation.Single
 
 @Single
 class TopRatedTvRemoteDataSourceImpl(
     private val topRatedTvSeriesApi: TopRatedTvSeriesApiService
-) : TopRatedTvRemoteDataSource {
+) : TopRatedTvRemoteDataSource, BaseRemoteDatasource {
     override suspend fun getTopRatedTvShows(
         pageNumber: Int,
-    ): ApiResponse<TopRatedTvSeriesRemote> =
-        topRatedTvSeriesApi.getTopRatedTvSeries(pageNumber)
+    ): Result<ApiResponse<TopRatedTvSeriesRemote>> =
+        callApi(
+            apiCall = { topRatedTvSeriesApi.getTopRatedTvSeries(pageNumber) },
+            mapper = { it }
+        )
 }
--- data/src/test/java/com/london/data/repository/TopRatedTvSeriesRepositoryImplTest.kt
+++ data/src/test/java/com/london/data/repository/TopRatedTvSeriesRepositoryImplTest.kt
@@ -29,7 +29,7 @@ class TopRatedTvSeriesRepositoryImplTest {
         // Given
         coEvery {
             remoteDataSource.getTopRatedTvShows(PAGE)
-        } returns fakeApiResponseWithTvSeries()
+        } returns Result.success(fakeApiResponseWithTvSeries())
 
         // When
         val result: List<TopRatedTvSeries> = repository.getTopRatedTvSeries(PAGE).items
@@ -53,7 +53,7 @@ class TopRatedTvSeriesRepositoryImplTest {
         // Given
         coEvery {
             remoteDataSource.getTopRatedTvShows(PAGE)
-        } returns fakeEmptyApiResponse()
+        } returns Result.success(fakeEmptyApiResponse())
 
         // When
         val result = repository.getTopRatedTvSeries(PAGE)
