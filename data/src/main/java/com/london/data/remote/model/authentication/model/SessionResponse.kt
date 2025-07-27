089e4d8a786b196079f96b2d1a85a9f183b8228c
Abdo Essam <Abdo.E.Mosa@gmail.com>
2025-07-27 21:08:50 +0300

    refactor/authentication (#300)
    * refactor: Remove validateSession from AuthRepository
    
    The `validateSession` function was removed from the
    `AuthRepository` interface and its implementation. This
    functionality is no longer needed as the session validation
    is handled differently. The corresponding tests were also
    removed.
    
    * Refactor: Improve LoginViewModel input handling
    
    - Introduce `limitTextFieldValue` for reusable input length restriction.
    - Remove unused import in LoginScreen.
    
    * Refactor: Relocate authentication data layer components
    
    This commit moves authentication-related classes within the data layer to a more specific `authentication` package.
    
    Specifically:
    - `AuthApiService` is renamed to `AuthenticationApiService` and moved to `data.remote.service.authentication`.
    - `AuthRepositoryImpl` is renamed to `AuthenticationRepositoryImpl` and moved to `data.repository.authentication`.
    - Data models previously under `data.remote.model.auth.model` are now under `data.remote.model.authentication.model`.
    - A new `AuthenticationRemoteDataSource` interface and its implementation `AuthenticationRemoteDataSourceImpl` are introduced in `data.remote.source.authentication`.
    - `NetworkModule` is updated to provide `AuthenticationApiService` instead of the old `AuthApiService`.
    - Unit tests for `AuthRepositoryImpl` are updated to reflect the new class name and location.
    
    This change aims to improve the organization and clarity of the authentication-related components within the data layer.
    
    * feat(data): Handle network exceptions in callApi
    
    This commit adds try-catch blocks to the callApi function in BaseRemoteDataSource.kt to handle UnknownHostException and SocketTimeoutException and return appropriate NetworkException results. Also, adds NoInternetException and UnknownException classes.
    
    * refactor(NetworkModule): Reorder imports for clarity
    
    * refactor: AuthRepositoryImplTest
    
    Refactor tests to use AuthenticationRemoteDataSource.

--- data/src/main/java/com/london/data/remote/model/authentication/model/SessionResponse.kt
+++ data/src/main/java/com/london/data/remote/model/authentication/model/SessionResponse.kt
@@ -1,4 +1,4 @@
-package com.london.data.remote.model.auth.model
+package com.london.data.remote.model.authentication.model
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
