c97249acee469f69b61d739ceb1fff10bb12ad6f
Marwan Qashwa <marawankashwa24@gmail.com>
2025-07-27 21:09:37 +0300

    fix: edit DefaultTopBar

--- presentation/src/main/java/com/london/presentation/feature/buildscreen/NetworkErrorScreen.kt
+++ presentation/src/main/java/com/london/presentation/feature/buildscreen/NetworkErrorScreen.kt
@@ -12,7 +12,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.london.designsystem.R
 import com.london.designsystem.component.Text
@@ -58,9 +57,4 @@ fun NetworkErrorScreen() {
             modifier = Modifier.padding(top = 16.dp)
         )
     }
-}
-@Preview
-@Composable
-fun prev(){
-    NetworkErrorScreen()
 }
