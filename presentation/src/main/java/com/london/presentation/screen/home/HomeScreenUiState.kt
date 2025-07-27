49c00f8b25d3fb86e7531a26b3d28ca8bb9fab42
Mostafa Mohamed <106277855+Mostafa-alsaygh@users.noreply.github.com>
2025-07-27 17:53:12 +0300

    refactor/presentation (#290)
    * Refactor: Move SplashRoute and improve code style
    
    * fix: adjust padding on Home screen

--- presentation/src/main/java/com/london/presentation/screen/home/HomeScreenUiState.kt
+++ presentation/src/main/java/com/london/presentation/screen/home/HomeScreenUiState.kt
@@ -14,6 +14,6 @@ data class HomeScreenUiState(
     val error: ErrorState? = null,
     val popularMovies: List<PopularMovie> = emptyList(),
     val popularTvShows: List<PopularTvShow> = emptyList(),
-    val upcomingMovies:  StateFlow<PagingData<Movie>> = MutableStateFlow<PagingData<Movie>>(PagingData.empty()),
+    val upcomingMovies: StateFlow<PagingData<Movie>> = MutableStateFlow(PagingData.empty()),
     val selectedGenre: MovieGenre = MovieGenre.All
 )
--- presentation/src/main/java/com/london/presentation/screen/login/LoginContract.kt
+++ presentation/src/main/java/com/london/presentation/screen/login/LoginContract.kt
@@ -3,13 +3,13 @@ package com.london.presentation.screen.login
 import androidx.compose.ui.text.input.TextFieldValue
 
 interface LoginContract {
- fun onUsernameChanged(username: TextFieldValue)
- fun onPasswordChanged(password: TextFieldValue)
- fun onPasswordVisibilityToggled()
- fun onLoginClick()
- fun onLoginAsGuestClick()
+    fun onUsernameChanged(username: TextFieldValue)
+    fun onPasswordChanged(password: TextFieldValue)
+    fun onPasswordVisibilityToggled()
+    fun onLoginClick()
+    fun onLoginAsGuestClick()
 
- fun onNavigateBack()
- fun onCreateAccountClick()
- fun onForgotPasswordClick()
+    fun onNavigateBack()
+    fun onCreateAccountClick()
+    fun onForgotPasswordClick()
 }
