5d927c40d1f82a79cd69fcde9b180d48cf0aa1e7
Asmaa.Khaled <asmaa.kh.gomaa@gmail.com>
2025-07-27 20:34:14 -0700

    fix: remove operator from invoke method

--- domain/src/main/java/com/london/domain/usecase/GetTrendingMoviesUseCase.kt
+++ domain/src/main/java/com/london/domain/usecase/GetTrendingMoviesUseCase.kt
@@ -8,6 +8,6 @@ import org.koin.core.annotation.Single
 
 @Single
 class GetTrendingMoviesUseCase(@Provided private val trendingRepository: TrendingRepository) {
-    suspend operator fun invoke(page: Int): PagedFetchResponse<Trending> =
+    suspend fun invoke(page: Int): PagedFetchResponse<Trending> =
         trendingRepository.getTrendingMovies(page)
 }
--- domain/src/main/java/com/london/domain/usecase/GetTrendingTvShowsUseCase.kt
+++ domain/src/main/java/com/london/domain/usecase/GetTrendingTvShowsUseCase.kt
@@ -8,6 +8,6 @@ import org.koin.core.annotation.Single
 
 @Single
 class GetTrendingTvShowsUseCase(@Provided private val trendingRepository: TrendingRepository) {
-    suspend operator fun invoke(page: Int): PagedFetchResponse<Trending> =
+    suspend fun invoke(page: Int): PagedFetchResponse<Trending> =
         trendingRepository.getTrendingTvShows(page)
 }
