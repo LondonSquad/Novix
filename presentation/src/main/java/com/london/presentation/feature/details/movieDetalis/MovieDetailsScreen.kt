ecd88e81242685dcbaf94d54b74e62883ea9507b
Abdulrahman1890 <Abdulrahmanmm2011@hotmail.com>
2025-07-29 22:04:44 +0300

    fix: issues from 43, 47, 48, 49, 58

--- presentation/src/main/java/com/london/presentation/feature/details/movieDetalis/MovieDetailsScreen.kt
+++ presentation/src/main/java/com/london/presentation/feature/details/movieDetalis/MovieDetailsScreen.kt
@@ -173,11 +173,6 @@ fun MovieDetailsContent(
                         .defaultMinSize(minHeight = 370.dp)
 
                 ) {
-                    MovieDetailsImage(
-                        images = state.movieImage,
-                        currentImageIndex = state.currentImageIndex,
-                        direction = state.imageSlideDirection,
-                    )
 
                     Column(
                         modifier = Modifier
@@ -514,7 +509,8 @@ private fun MovieDetailsImage(
                             .fillMaxSize()
                             .clip(RoundedCornerShape(12.dp)),
                         onLoadingStateChange = { loadingState.value = it },
-                        errorContent = { ErrorImage() })
+                        errorContent = { ErrorImage() }
+                    )
                 }
             }
         } else {
--- presentation/src/main/java/com/london/presentation/shared/CustomBackDropImagePager.kt
+++ presentation/src/main/java/com/london/presentation/shared/CustomBackDropImagePager.kt
@@ -14,6 +14,8 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -43,6 +45,7 @@ fun CustomBackDropImagePager(
                 .background(NovixTheme.colors.surface),
             contentAlignment = Alignment.Center
         ) {
+            ImageVerticalGradient()
             ErrorImage()
         }
     } else {
@@ -109,6 +112,30 @@ fun CustomBackDropImagePager(
                         .align(Alignment.BottomCenter)
                 )
             }
+            ImageVerticalGradient()
         }
     }
+}
+
+@Composable
+private fun ImageVerticalGradient() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(252.dp)
+            .clip(
+                shape = RoundedCornerShape(
+                    bottomStart = 12.dp,
+                    bottomEnd = 12.dp
+                )
+            )
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        Color(0xff000000).copy(alpha = 0.5f),
+                        Color(0xff000000).copy(alpha = 0f)
+                    )
+                )
+            )
+    )
 }
