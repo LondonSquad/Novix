c4f43e3f6e5f67f38329467c55bd2db652d2d6d4
Asmaa.Khaled <asmaa.kh.gomaa@gmail.com>
2025-07-30 09:44:04 -0700

    feat: implement retry functionality for trending actors

--- presentation/src/main/java/com/london/presentation/feature/home/trending/actor/TrendingActorsContract.kt
+++ presentation/src/main/java/com/london/presentation/feature/home/trending/actor/TrendingActorsContract.kt
@@ -3,9 +3,11 @@ package com.london.presentation.feature.home.trending.actor
 interface TrendingActorsContract {
     fun onActorClick(id: Int)
     fun onBack()
+    fun onRetry()
 }
 
 fun defaultTrendingActorsContract() = object : TrendingActorsContract {
     override fun onActorClick(id: Int) {}
     override fun onBack() {}
+    override fun onRetry() {}
 }
