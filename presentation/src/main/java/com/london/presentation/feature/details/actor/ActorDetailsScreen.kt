80a95517e2e581d7454e851eadd5051a2767e1a8
Yusuf Nasser <YusufNasserEng@gmail.com>
2025-08-01 02:20:26 +0300

    Revert "Add extension to trim excessive spaces"
    This reverts commit d7f5562da7bd5e7bdadd1fe0b614968201378b3e.

--- presentation/src/main/java/com/london/presentation/feature/details/actor/ActorDetailsScreen.kt
+++ presentation/src/main/java/com/london/presentation/feature/details/actor/ActorDetailsScreen.kt
@@ -66,7 +66,6 @@ import com.london.presentation.shared.CustomBackDropImagePager
 import com.london.presentation.utils.Listen
 import com.london.presentation.utils.offsetLayout
 import com.london.presentation.utils.toLocalizedNumbers
-import com.london.presentation.utils.trimExcessiveSpaces
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -238,7 +237,7 @@ private fun BiographySection(uiState: ActorDetailsUiState) {
         var isExpanded by remember { mutableStateOf(false) }
 
         ConditionalText(
-            text = uiState.actorBiography.trimExcessiveSpaces(),
+            text = uiState.actorBiography,
             expandedState = isExpanded,
             modifier = Modifier.padding(horizontal = 16.dp, vertical = 4.dp)
         ) {
