efc0d1dcb287921732035fdd4fa06da99bc86d6a
AbdulrahmanMagdey <abdomagdey5@gmail.com>
2025-07-31 15:49:11 +0300

    refactor: removed unneeded import

--- presentation/src/main/java/com/london/presentation/feature/login/LoginScreen.kt
+++ presentation/src/main/java/com/london/presentation/feature/login/LoginScreen.kt
@@ -36,7 +36,6 @@ import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.london.designsystem.component.Icon
