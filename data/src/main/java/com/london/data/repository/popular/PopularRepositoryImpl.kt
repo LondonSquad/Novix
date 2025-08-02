477800af814e9c9e2129e8ffa91d27379959d946
ahmbd <ahmbdelkhalek@gmail.com>
2025-08-02 23:49:47 +0300

    feat: Implement caching for popular movies

--- data/src/main/java/com/london/data/repository/popular/PopularRepositoryImpl.kt
+++ data/src/main/java/com/london/data/repository/popular/PopularRepositoryImpl.kt
@@ -3,6 +3,7 @@ package com.london.data.repository.popular
 import com.london.data.local.model.home.popular.PopularSectionLocal
 import com.london.data.local.source.HomeLocalDataSource
 import com.london.data.mapper.popular.toEntity
+import com.london.data.mapper.popular.toEntityList
 import com.london.data.mapper.popular.toPopularMovies
 import com.london.data.mapper.popular.toPopularSectionLocal
 import com.london.data.remote.source.home.popular.PopularRemoteDataSource
@@ -10,6 +11,7 @@ import com.london.data.utils.CrashReporter
 import com.london.data.utils.fetchAndSync
 import com.london.domain.entity.popular.PopularMovie
 import com.london.domain.entity.popular.PopularTvShow
+import com.london.domain.entity.recent.MediaType
 import com.london.domain.repository.PopularRepository
 import javax.inject.Inject
 
@@ -23,19 +25,23 @@ class PopularRepositoryImpl @Inject constructor(
 //        return popularRemoteDataSource.getPopularMovies().getOrThrow().toPopularMovies()
 //    }
 //
-//    override suspend fun getPopularTvShows(): List<PopularTvShow> {
-//        return popularRemoteDataSource.getPopularTvShows().getOrThrow().toEntityList()
-//    }
-    override suspend fun getPopularMovies(): List<PopularMovie> = fetchAndSync(
+    override suspend fun getPopularTvShows(): List<PopularTvShow> {
+        return popularRemoteDataSource.getPopularTvShows().getOrThrow().toEntityList()
+    }
+    override suspend fun getPopularMovies(
+    ): List<PopularMovie> = fetchAndSync(
         cacheBlock = { homeLocalDataSource.getAll().map { it.toEntity() } },
-        networkBlock = { popularRemoteDataSource.getPopularMovies().getOrThrow().toPopularMovies() },
+        networkBlock = {
+            popularRemoteDataSource.getPopularMovies().getOrThrow().toPopularMovies()
+        },
         syncBlock = { popularList ->
-            homeLocalDataSource.insertAll(popularList.map { it.toPopularSectionLocal(popularList) }) },
+            homeLocalDataSource.insertAll(popularList.map { it.toPopularSectionLocal(MediaType.Movie) })
+        },
         crashReporter = crashReporter
     )
 
-    override suspend fun getPopularTvShows(): List<PopularTvShow> {
-        TODO("Not yet implemented")
-    }
+//    override suspend fun getPopularTvShows(): List<PopularTvShow> {
+//
+//    }
 
 }
