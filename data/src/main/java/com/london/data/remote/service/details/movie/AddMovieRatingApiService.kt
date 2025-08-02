a10054f5c4f2c543ba7ac1a86c263718f1039bc7
abdelrhman1002 <abdelrhmanshabana999@gmail.com>
2025-08-02 15:42:53 +0300

    feature/add-movie-rating-data: Implement movie rating API (#505)
    and movie states

---
+++ data/src/main/java/com/london/data/remote/service/details/movie/AddMovieRatingApiService.kt
@@ -0,0 +1,19 @@
+package com.london.data.remote.service.details.movie
+
+import com.london.data.remote.model.details.rating.RatingRemoteBody
+import com.london.data.remote.model.details.rating.RatingRemoteResponse
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface AddMovieRatingApiService {
+    @POST("3/movie/{movie_id}/rating")
+    suspend fun getAccountMovieStates(
+        @Path("movie_id") movieId: Int,
+        @Query("guest_session_id") guestSessionId: String?,
+        @Query("session_id") userSessionId: String?,
+        @Body ratingRequest: RatingRemoteBody
+    ): Response<RatingRemoteResponse>
+}
--- data/src/main/java/com/london/data/remote/source/details/movie/MovieDetailsRemoteDataSourceImpl.kt
+++ data/src/main/java/com/london/data/remote/source/details/movie/MovieDetailsRemoteDataSourceImpl.kt
@@ -2,6 +2,7 @@ package com.london.data.remote.source.details.movie
 
 import com.london.data.remote.model.ApiResponse
 import com.london.data.remote.model.details.movie.model.moviecast.MovieCastResponse
+import com.london.data.remote.model.details.movie.model.moviedetails.AccountMovieStatesResponse
 import com.london.data.remote.model.details.movie.model.moviedetails.MovieDetailsResponse
 import com.london.data.remote.model.details.movie.model.movieimages.MovieImagesResponse
 import com.london.data.remote.model.search.model.SearchMovieRemote
@@ -15,24 +16,33 @@ class MovieDetailsRemoteDataSourceImpl @Inject constructor(
     override suspend fun getMovieDetails(movieId: Int): Result<MovieDetailsResponse> =
         callApiWithRetry(
             apiCall = { movieDetailsApiService.getMovieDetails(movieId = movieId) },
-            mapper = { it }
-        )
+            mapper = { it })
 
     override suspend fun getSimilarMovies(movieId: Int): Result<ApiResponse<SearchMovieRemote>> =
         callApiWithRetry(
             apiCall = { movieDetailsApiService.getSimilarMovies(movieId = movieId) },
-            mapper = { it }
-        )
+            mapper = { it })
 
-    override suspend fun getMovieCast(movieId: Int): Result<MovieCastResponse> =
-        callApiWithRetry(
-            apiCall = { movieDetailsApiService.getMovieCast(movieId = movieId) },
-            mapper = { it }
-        )
+    override suspend fun getMovieCast(movieId: Int): Result<MovieCastResponse> = callApiWithRetry(
+        apiCall = { movieDetailsApiService.getMovieCast(movieId = movieId) },
+        mapper = { it })
 
     override suspend fun getMovieImages(movieId: Int): Result<MovieImagesResponse> =
         callApiWithRetry(
             { movieDetailsApiService.getMovieImages(movieId = movieId) },
-            mapper = { it }
-        )
+            mapper = { it })
+
+    override suspend fun getAccountMovieStates(
+        movieId: Int,
+        guestSessionId: String?,
+        userSessionId: String?
+    ): Result<AccountMovieStatesResponse> =
+        callApiWithRetry(apiCall = {
+            movieDetailsApiService.getMovieStates(
+                movieId = movieId,
+                guestSessionId = guestSessionId,
+                userSessionId = userSessionId
+            )
+        }, mapper = { it })
+
 }
