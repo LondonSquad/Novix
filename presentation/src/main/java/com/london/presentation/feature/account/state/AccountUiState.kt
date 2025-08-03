3195ec4837b842196a4c193052c6fb9320bc3a8c
abdo-essam <abdo.e.mosa@gmail.com>
2025-08-03 20:38:47 +0300

    feat(account): Set default values for AccountUiState

--- presentation/src/main/java/com/london/presentation/feature/account/state/AccountUiState.kt
+++ presentation/src/main/java/com/london/presentation/feature/account/state/AccountUiState.kt
@@ -5,9 +5,9 @@ import com.london.presentation.feature.base.ErrorState
 data class AccountUiState(
     val isLoading: Boolean = false,
     val error: ErrorState? = null,
-    val username: String,
-    val currentAppearance: String,
-    val currentLanguage: String,
+    val username: String = "",
+    val currentAppearance: String = "",
+    val currentLanguage: String = "",
     val isUserLoggedIn: Boolean = true,
     val showUserMenu: Boolean = false,
     val showContentRestrictionBottomSheet: Boolean = false,
