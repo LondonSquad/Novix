0df33ffe7e4f3d6464fc65bddfdf61aaae1be4e4
Abdulrahman1890 <Abdulrahmanmm2011@hotmail.com>
2025-08-03 03:13:21 +0300

    feat: caching the popular section

--- data/src/main/java/com/london/data/local/source/home/popular/PopularLocalDataSourceImpl.kt
+++ data/src/main/java/com/london/data/local/source/home/popular/PopularLocalDataSourceImpl.kt
@@ -9,7 +9,6 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-
 class PopularLocalDataSourceImpl @Inject constructor(
     private val popularSectionDao: PopularSectionDao
 ) : HomeLocalDataSource<PopularSectionLocal> {
