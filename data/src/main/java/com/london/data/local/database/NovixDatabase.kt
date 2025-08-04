78d18589f89104cecc9acd407b5b4bf9e324e105
Aseel AbuKmail <aseelabukmail@gmail.com>
2025-08-05 00:24:20 +0300

    Feature/remove search caching (#526)
    * feat: remove caching for search
    
    * feat: remove search DAO and related DTOs
    
    * refactor: remove unused search-related mappers
    
    * refactor: remove unused imports in RepositoryModule
    
    * fix/search-caching-error (#524)
    
    * fix:
    - remove unused convertors for search
    - remove duplicate popular local from novix database
    - update genre insert entity in novix databse
    
    * fix: add genre interest DAO to database module
    
    * feature/merge-remove-search-caching-with-develop (#525)
    
    * refactor/data_packaging (#521)
    
    * refactor: update authentication model imports
    
    * refactor: remove unused API path functions
    
    * refactor:
    - reorganize top-rated model imports
    - remove reviews dto local it was not used
    - rearrange authentication preferences
    
    * refactor/packaging: rename and reorganize repository files
    
    ---------
    
    Co-authored-by: london-bot[bot] <220529784+london-bot[bot]@users.noreply.github.com>
    
    * fix: remove unused search DAOs from NovixDatabase
    
    * fix: remove unused imports in search repository
    
    ---------
    
    Co-authored-by: london-bot[bot] <220529784+london-bot[bot]@users.noreply.github.com>
    
    * fix: remove unused import
    
    * refactor: remove local data source unit tests and refactor SearchRepositoryImplTest
    
    ---------
    
    Co-authored-by: Bassant Mohammed <bassant.22230@gmail.com>
    Co-authored-by: Bassant Mohammed <102248144+BassantM@users.noreply.github.com>
    Co-authored-by: london-bot[bot] <220529784+london-bot[bot]@users.noreply.github.com>

--- data/src/main/java/com/london/data/local/database/NovixDatabase.kt
+++ data/src/main/java/com/london/data/local/database/NovixDatabase.kt
@@ -6,9 +6,6 @@ import androidx.room.TypeConverters
 import com.london.data.local.database.convertor.CommonConverter
 import com.london.data.local.database.convertor.MediaTypeConverter
 import com.london.data.local.database.convertor.RecentViewedConverter
-import com.london.data.local.database.convertor.SearchActorsConvertor
-import com.london.data.local.database.convertor.SearchMoviesConverter
-import com.london.data.local.database.convertor.SearchTvShowConvertor
 import com.london.data.local.database.convertor.UpComingMovieTypeConverter
 import com.london.data.local.database.dao.home.popular.PopularSectionDao
 import com.london.data.local.database.dao.home.toprated.TopRatedDao
@@ -18,9 +15,6 @@ import com.london.data.local.database.dao.recent.viewed.RecentViewedDao
 import com.london.data.local.database.dao.recent.whatched.movie.RecentWatchedMoviesDao
 import com.london.data.local.database.dao.recent.whatched.tvshow.RecentWatchedTvShowsDao
 import com.london.data.local.database.dao.search.GenreInterestDao
-import com.london.data.local.database.dao.search.SearchActorsDao
-import com.london.data.local.database.dao.search.SearchMoviesDao
-import com.london.data.local.database.dao.search.SearchTvShowDao
 import com.london.data.local.model.home.popular.PopularSectionLocal
 import com.london.data.local.model.home.topRated.TopRatedLocal
 import com.london.data.local.model.home.upcoming.UpComingSectionLocal
@@ -29,46 +23,34 @@ import com.london.data.local.model.recent.viewed.RecentViewedLocal
 import com.london.data.local.model.recent.watched.RecentWatchedMovieLocal
 import com.london.data.local.model.recent.watched.RecentWatchedTvShowLocal
 import com.london.data.local.model.search.GenreInterestEntity
-import com.london.data.local.model.search.SearchActorsLocal
-import com.london.data.local.model.search.SearchMoviesLocal
-import com.london.data.local.model.search.SearchTvShowLocal
 
 @Database(
     entities = [
-        SearchTvShowLocal::class,
-        SearchMoviesLocal::class,
-        SearchActorsLocal::class,
         RecentSearchLocal::class,
-        GenreInterestEntity::class,
         RecentViewedLocal::class,
         RecentWatchedMovieLocal::class,
         RecentWatchedTvShowLocal::class,
-        PopularSectionLocal::class,
         TopRatedLocal::class,
-        UpComingSectionLocal::class
+        UpComingSectionLocal::class,
+        PopularSectionLocal::class,
+        GenreInterestEntity::class
     ],
     version = 1,
     exportSchema = false
 )
 @TypeConverters(
-    SearchActorsConvertor::class,
-    SearchMoviesConverter::class,
-    SearchTvShowConvertor::class,
     CommonConverter::class,
     RecentViewedConverter::class,
     MediaTypeConverter::class,
     UpComingMovieTypeConverter::class
 )
 abstract class NovixDatabase : RoomDatabase() {
-    abstract fun searchTvShowDao(): SearchTvShowDao
-    abstract fun searchMoviesDao(): SearchMoviesDao
-    abstract fun searchActorsDao(): SearchActorsDao
     abstract fun recentSearchDao(): RecentSearchDao
-    abstract fun genreInterestDao(): GenreInterestDao
     abstract fun recentViewedDao(): RecentViewedDao
     abstract fun recentWatchedMoviesDao(): RecentWatchedMoviesDao
     abstract fun recentWatchedTvShowsDao(): RecentWatchedTvShowsDao
     abstract fun popularSectionDao(): PopularSectionDao
     abstract fun upComingSectionDao(): UpcomingSectionDao
     abstract fun topRatedDao(): TopRatedDao
+    abstract fun genreInterestDao(): GenreInterestDao
 }
