199930a3518310eb8e7d47a4cb47f25c5bfd2d32
Bassant Mohammed <bassant.22230@gmail.com>
2025-08-04 21:33:42 +0300

    fix: remove unused search DAOs from NovixDatabase

--- data/src/main/java/com/london/data/local/database/NovixDatabase.kt
+++ data/src/main/java/com/london/data/local/database/NovixDatabase.kt
@@ -15,9 +15,6 @@ import com.london.data.local.database.dao.recent.viewed.RecentViewedDao
 import com.london.data.local.database.dao.recent.whatched.movie.RecentWatchedMoviesDao
 import com.london.data.local.database.dao.recent.whatched.tvshow.RecentWatchedTvShowsDao
 import com.london.data.local.database.dao.search.GenreInterestDao
-import com.london.data.local.database.dao.search.SearchActorsDao
-import com.london.data.local.database.dao.search.SearchMoviesDao
-import com.london.data.local.database.dao.search.SearchTvShowDao
 import com.london.data.local.model.home.popular.PopularSectionLocal
 import com.london.data.local.model.home.topRated.TopRatedLocal
 import com.london.data.local.model.home.upcoming.UpComingSectionLocal
