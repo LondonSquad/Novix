c032ccc87ea86b54066e264ffce75541dfd2a05c
Bassant Mohammed <bassant.22230@gmail.com>
2025-08-04 17:55:37 +0300

    refactor: update authentication model imports

--- data/src/main/java/com/london/data/remote/model/authentication/LoginValidationRequestBody.kt
+++ data/src/main/java/com/london/data/remote/model/authentication/LoginValidationRequestBody.kt
@@ -1,4 +1,4 @@
-package com.london.data.remote.model.authentication.model
+package com.london.data.remote.model.authentication
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
