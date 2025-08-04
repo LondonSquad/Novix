4a8b2f11a930d15a2c12dff529ab803e28b86bbe
Asmaa.Khaled <asmaa.kh.gomaa@gmail.com>
2025-08-05 00:57:22 +0300

    refactor: Remove ContinueWatching feature

--- presentation/src/main/java/com/london/presentation/feature/continuewatching/ContinueWatchingEffect.kt
+++
@@ -1,7 +0,0 @@
-package com.london.presentation.feature.continuewatching
-
-sealed interface ContinueWatchingEffect {
-    data object NavigateBack : ContinueWatchingEffect
-    data class NavigateToMovieDetails(val id: Int) : ContinueWatchingEffect
-    data class NavigateToTvShowDetails(val id: Int) : ContinueWatchingEffect
-}
