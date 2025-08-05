9d3a119922ca8071d980b9b944d39147d2949a9c
Yusuf Nasser <yusufnassereng@gmail.com>
2025-08-05 09:00:20 +0300

    refactor: update RecentWatchedRepository to return Flow
    The `getAllRecentWatchedMovies` and `getAllRecentWatchedTvShows` methods in the `RecentWatchedRepository` interface have been updated to return `Flow<List<Movie>>` and `Flow<List<TvShow>>` respectively. This change allows for asynchronous data streams, enabling observers to react to changes in the list of recently watched movies and TV shows.

--- domain/src/main/java/com/london/domain/repository/RecentWatchedRepository.kt
+++ domain/src/main/java/com/london/domain/repository/RecentWatchedRepository.kt
@@ -4,10 +4,11 @@ package com.london.domain.repository
 import com.london.domain.KoverIgnore
 import com.london.domain.entity.Movie
 import com.london.domain.entity.TvShow
+import kotlinx.coroutines.flow.Flow
 
 interface RecentWatchedRepository {
-    suspend fun getAllRecentWatchedMovies(): List<Movie>
+    suspend fun getAllRecentWatchedMovies(): Flow<List<Movie>>
     suspend fun insertMovie(item: Movie)
-    suspend fun getAllRecentWatchedTvShows(): List<TvShow>
+    suspend fun getAllRecentWatchedTvShows(): Flow<List<TvShow>>
     suspend fun insertTvShow(item: TvShow)
 }
