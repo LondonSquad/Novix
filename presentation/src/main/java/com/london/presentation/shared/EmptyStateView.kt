8d8ef3f5300bec4c63e71e77354d4a528505e1e9
Bassant Mohammed <bassant.22230@gmail.com>
2025-08-05 20:09:47 +0300

    feat: update empty state visuals and strings by new design

--- presentation/src/main/java/com/london/presentation/shared/EmptyStateView.kt
+++ presentation/src/main/java/com/london/presentation/shared/EmptyStateView.kt
@@ -1,11 +1,13 @@
 package com.london.presentation.shared
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import com.london.designsystem.component.Text
 import com.london.designsystem.theme.NovixTheme
@@ -20,9 +22,13 @@ fun EmptyStateView() {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
+        Image(
+            painter = painterResource(id = R.drawable.empty),
+            contentDescription = null
+        )
         Text(
-            text = stringResource(R.string.no_data_until_now),
-            style = NovixTheme.typography.label.large,
+            text = stringResource(R.string.there_is_no_items_for_this_genre),
+            style = NovixTheme.typography.body.small,
             color = NovixTheme.colors.body
         )
     }
