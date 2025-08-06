30c2ccb217d3d6ab78e9ad7264d27ea93227a0bc
Asmaa.Khaled <asmaa.kh.gomaa@gmail.com>
2025-08-06 12:52:37 +0300

    feat: Implement MediaGenreFilters composable

---
+++ presentation/src/main/java/com/london/presentation/shared/MediaGenreFilters.kt
@@ -0,0 +1,86 @@
+package com.london.presentation.shared
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.london.designsystem.component.NovixChip
+import com.london.designsystem.theme.ThemePreviews
+import com.london.presentation.utils.MovieGenre
+import com.london.presentation.utils.TvShowGenre
+
+@Composable
+fun MediaGenreFilters(
+    isMovieSelected: Boolean,
+    isTvShowSelected: Boolean,
+    selectedMovieGenre: MovieGenre,
+    selectedTvShowGenre: TvShowGenre,
+    onMovieGenreClick: (MovieGenre) -> Unit,
+    onTvShowGenreClick: (TvShowGenre) -> Unit,
+    screenWidth: Dp
+) {
+    when {
+        isMovieSelected -> GenreChipsRow(
+            genres = MovieGenre.entries,
+            selectedGenre = selectedMovieGenre,
+            onGenreClick = onMovieGenreClick,
+            screenWidth = screenWidth,
+            getText = { stringResource(it.stringResId) }
+        )
+
+        isTvShowSelected -> GenreChipsRow(
+            genres = TvShowGenre.entries,
+            selectedGenre = selectedTvShowGenre,
+            onGenreClick = onTvShowGenreClick,
+            screenWidth = screenWidth,
+            getText = { stringResource(it.stringResId) }
+        )
+    }
+}
+
+@Composable
+inline fun <reified T : Enum<T>> GenreChipsRow(
+    genres: List<T>,
+    selectedGenre: T,
+    crossinline onGenreClick: (T) -> Unit,
+    screenWidth: Dp,
+    noinline getText: @Composable (T) -> String,
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
+            .requiredWidth(screenWidth)
+            .padding(vertical = 12.dp)
+    ) {
+        items(genres) { genre ->
+            NovixChip(
+                text = getText(genre),
+                isSelected = genre == selectedGenre,
+                onClick = { onGenreClick(genre) }
+            )
+        }
+    }
+}
+
+@ThemePreviews
+@Composable
+private fun Preview() {
+    MediaGenreFilters(
+        isMovieSelected = true,
+        isTvShowSelected = false,
+        selectedMovieGenre = MovieGenre.All,
+        selectedTvShowGenre = TvShowGenre.All,
+        onMovieGenreClick = {},
+        onTvShowGenreClick = {},
+        screenWidth = 360.dp
+    )
+}
