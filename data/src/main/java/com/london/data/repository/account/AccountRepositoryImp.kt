0b5a040360cca85db1ea1e5d5387767851127fa6
Asmaa.Khaled <asmaa.kh.gomaa@gmail.com>
2025-08-08 01:48:24 +0300

    refactor: Modify AccountRepositoryImp to return AccountInfo

--- data/src/main/java/com/london/data/repository/account/AccountRepositoryImp.kt
+++ data/src/main/java/com/london/data/repository/account/AccountRepositoryImp.kt
@@ -3,6 +3,7 @@ package com.london.data.repository.account
 import com.london.data.local.preference.AuthPreferences
 import com.london.data.mapper.account.toEntity
 import com.london.data.remote.source.account.AccountRemoteDataSource
+import com.london.domain.entity.AccountInfo
 import com.london.domain.repository.AccountRepository
 import javax.inject.Inject
 
@@ -10,11 +11,10 @@ class AccountRepositoryImp @Inject constructor(
     private val accountRemoteDataSource: AccountRemoteDataSource,
     private val authPreferences: AuthPreferences
 ) : AccountRepository {
-    override suspend fun getUserName(): String =
+    override suspend fun getAccountDetails(): AccountInfo =
         authPreferences.getSessionId()?.let { sessionId ->
-            accountRemoteDataSource.getUserName(sessionId)
+            accountRemoteDataSource.getAccountDetails(sessionId)
                 .getOrThrow()
                 .toEntity()
-                .userName
-        } ?: ""
+        } ?: AccountInfo(id = 0, userName = "", avatarPath = "")
 }
