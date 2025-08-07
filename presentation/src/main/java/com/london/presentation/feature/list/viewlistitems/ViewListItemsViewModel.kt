9b40115a79ffb560bf4bc04d625c6c267dc70761
Yusuf Nasser <yusufnassereng@gmail.com>
2025-08-08 02:57:39 +0300

    fix: handle delete bottom sheet dismiss and error state

--- presentation/src/main/java/com/london/presentation/feature/list/viewlistitems/ViewListItemsViewModel.kt
+++ presentation/src/main/java/com/london/presentation/feature/list/viewlistitems/ViewListItemsViewModel.kt
@@ -27,7 +27,6 @@ class ViewListItemsViewModel @Inject constructor(
     private val listId = args?.listId ?: 0
 
     init {
-        
         getMovieListName(listId = listId)
         fetchMovieListDetails(listId = listId)
     }
@@ -58,7 +57,7 @@ class ViewListItemsViewModel @Inject constructor(
                 manageMovieListUseCase.deleteMovieList(listId.toUInt())
             },
             onError = {
-                updateState { copy(error = ErrorState.EntryNotFound()) }
+                updateState { copy(error = ErrorState.RequestFailed()) }
             },
             onSuccess = {
                 emitEffect(ViewListItemsEffect.NavigateBack)
@@ -87,6 +86,10 @@ class ViewListItemsViewModel @Inject constructor(
                 updateState { copy(isSnackBarSuccessVisible = true) }
             }
         )
+    }
+
+    override fun onDeleteBottomSheetDismiss() {
+        updateState { copy(isDeleteBottomSheetVisible = false) }
     }
 
     private fun fetchMovieListDetails(listId: Int) {
