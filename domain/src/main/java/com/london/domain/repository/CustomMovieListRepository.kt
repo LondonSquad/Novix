ed3215f5e0a69bce45aafb7d6117788c970e6c5a
Marwan Qashwa <100484790+marwanqashwa@users.noreply.github.com>
2025-08-07 04:20:31 +0300

    Feature/custom list data (#547)
    * feat: init custom lists data
    
    * refactor: Rename `MovieListRepository` to `CustomMovieListRepository`
    
    * refactor: rename movie list related classes and interfaces
    
    * feat: add custom movie list response and mapper
    
    * feat: Implement `getAllUserLists` endpoint in `CustomMovieListsApiService`
    
    * refactor: inject `AuthPreferences` and pass session ID to custom movie list API calls
    
    * feat: add language support to custom movie list creation
    
    * feat: implement CustomMovieListsRemoteDataSource
    
    * feat: Implement pagination for movie list retrieval
    
    * feat: add page number parameter to movie list use cases & update their tests
    
    * feat: provide `CustomMovieLists` dependencies
    
    * fix: update data layer for list details
    
    * fix: handle add movie to list with retry, and delete list
    
    * fix: add unit test for repo
    
    * fix: update data for list details
    
    * fix: handle add movie to list with retry, and delete list
    
    * fix: add unit test for repo
    
    * fix: update test naming
    
    * fix: update get all listed movies
    
    ---------
    
    Co-authored-by: Yusuf Nasser <yusufnassereng@gmail.com>
    Co-authored-by: london-bot[bot] <220529784+london-bot[bot]@users.noreply.github.com>

--- domain/src/main/java/com/london/domain/repository/CustomMovieListRepository.kt
+++ domain/src/main/java/com/london/domain/repository/CustomMovieListRepository.kt
@@ -4,11 +4,11 @@ import com.london.domain.entity.Movie
 import com.london.domain.entity.MovieList
 import com.london.domain.entity.PagedFetchResponse
 
-interface MovieListRepository {
+interface CustomMovieListRepository {
 
     suspend fun deleteMovieList(id: UInt): Boolean
     suspend fun createMovieList(name: String): Boolean
-    suspend fun getMovieLists(): PagedFetchResponse<MovieList>
+    suspend fun getMovieLists(pageNumber: Int): PagedFetchResponse<MovieList>
     suspend fun addMovieToList(listId: UInt, movieId: UInt): Boolean
-    suspend fun getMovieListDetails(listId: UInt): PagedFetchResponse<Movie>
+    suspend fun getMovieListDetails(listId: UInt, pageNumber: Int): PagedFetchResponse<Movie>
 }
