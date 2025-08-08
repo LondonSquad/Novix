d5b3e156a84657fb752bf838701a5648e0b7aae5
Mostafa Mohammed <tofymofy.55@gmail.com>
2025-08-08 03:40:31 +0300

    Fix: Adjust top bar padding to accommodate status bar

--- presentation/src/main/java/com/london/presentation/shared/MediaLazyGrid.kt
+++ presentation/src/main/java/com/london/presentation/shared/MediaLazyGrid.kt
@@ -5,11 +5,13 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -45,8 +47,10 @@ fun <T> MediaLazyGrid(
         TopBar(
             modifier = Modifier
                 .fillMaxWidth()
-                .statusBarsPadding()
-                .padding(horizontal = 16.dp),
+                .padding(horizontal = 16.dp)
+                .padding(
+                    top = WindowInsets.statusBars.asPaddingValues().calculateTopPadding() + 12.dp
+                ),
             title = title,
             onBackClick = onBack
         )
