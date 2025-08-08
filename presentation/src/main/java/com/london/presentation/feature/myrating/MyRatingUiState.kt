5c7fbb706290b767cc3bb6f90da7ef395361038a
Asmaa.Khaled <asmaa.kh.gomaa@gmail.com>
2025-08-08 07:51:25 +0300

    refactor: Use AllRatedContent in MyRatingUiState

--- presentation/src/main/java/com/london/presentation/feature/myrating/MyRatingUiState.kt
+++ presentation/src/main/java/com/london/presentation/feature/myrating/MyRatingUiState.kt
@@ -1,5 +1,6 @@
 package com.london.presentation.feature.myrating
 
+import com.london.domain.entity.myrating.AllRatedContent
 import com.london.domain.entity.myrating.RatedMovie
 import com.london.domain.entity.myrating.RatedTvShow
 import com.london.presentation.shared.base.ErrorState
@@ -7,7 +8,7 @@ import com.london.presentation.shared.base.ErrorState
 data class MyRatingUiState(
     val movies: List<RatedMovie> = emptyList(),
     val tvShows: List<RatedTvShow> = emptyList(),
-    val allRated: Pair<List<RatedTvShow>, List<RatedMovie>> = Pair(emptyList(), emptyList()),
+    val allRated: AllRatedContent = AllRatedContent(emptyList(), emptyList()),
     val errorState: ErrorState? = null,
     val isLoading: Boolean = false,
     val rate: Int = 0,
