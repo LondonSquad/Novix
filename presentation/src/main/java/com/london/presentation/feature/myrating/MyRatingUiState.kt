72833fc2795fd134f41e34a417976c378b1b8cbc
Asmaa.Khaled <asmaa.kh.gomaa@gmail.com>
2025-08-08 05:30:48 +0300

    feat: Add selected rating category to UI state

--- presentation/src/main/java/com/london/presentation/feature/myrating/MyRatingUiState.kt
+++ presentation/src/main/java/com/london/presentation/feature/myrating/MyRatingUiState.kt
@@ -7,11 +7,12 @@ import com.london.presentation.shared.base.ErrorState
 data class MyRatingUiState(
     val movies: List<RatedMovie> = emptyList(),
     val tvShows: List<RatedTvShow> = emptyList(),
-    val allRated: Pair<List<RatedTvShow>,List<RatedMovie>> = Pair(emptyList(), emptyList()),
+    val allRated: Pair<List<RatedTvShow>, List<RatedMovie>> = Pair(emptyList(), emptyList()),
     val errorState: ErrorState? = null,
     val isLoading: Boolean = false,
     val rate: Int = 0,
     val isDeleteClicked: Boolean = false,
     val backdropPath: String = "",
     val id: Int = 0,
+    val selectedRatingCategory: RatingCategory? = null,
 )
