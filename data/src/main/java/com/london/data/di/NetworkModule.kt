b46e467561674035f125e2d5c81c920b7e3f0f70
Karim Al-Zmzmy <karam.zmzmy@gmail.com>
2025-08-08 05:44:37 +0300

    refactor: use `readLanguageCode` in `NetworkModule`

--- data/src/main/java/com/london/data/di/NetworkModule.kt
+++ data/src/main/java/com/london/data/di/NetworkModule.kt
@@ -5,7 +5,7 @@ import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFact
 import com.london.data.BuildConfig
 import com.london.data.local.preference.AuthPreferences
 import com.london.data.local.preference.SharedPrefsTokenProvider
-import com.london.data.local.source.device.DeviceConfigurationDataSource
+import com.london.data.local.preference.readLanguageCode
 import com.london.data.remote.interceptor.AuthInterceptor
 import com.london.data.remote.service.account.AccountApiService
 import com.london.data.remote.service.authentication.AuthenticationApiService
@@ -67,7 +67,7 @@ object NetworkModule {
     fun provideApiInterceptor(@ApplicationContext context: Context): Interceptor =
         Interceptor { chain ->
             val original = chain.request()
-            val deviceLanguage = DeviceConfigurationDataSource(context).getCurrentLanguage()
+            val deviceLanguage = readLanguageCode(context)
 
             val newUrl = original.url.newBuilder()
                 .addQueryParameter("api_key", BuildConfig.API_KEY)
