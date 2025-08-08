1857d028b50ef5542963fd98ae5b197a3e189054
Asmaa.Khaled <asmaa.kh.gomaa@gmail.com>
2025-08-08 14:23:41 +0300

    refactor: Delete `MediaItem` data class and associated mappers

--- domain/src/main/java/com/london/domain/entity/myrating/MediaItem.kt
+++
@@ -1,9 +0,0 @@
-package com.london.domain.entity.myrating
-
-data class MediaItem(
-    val id: Int,
-    val posterPath: String,
-    val title: String,
-    val rating: Int,
-    val isMovie: Boolean
-) 
