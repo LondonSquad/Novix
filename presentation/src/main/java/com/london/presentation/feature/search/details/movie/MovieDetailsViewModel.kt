3c3315ef14a80371fa9cab3bad060b9f88541db7
abdelrhman1002 <abdelrhmanshabana999@gmail.com>
2025-08-10 03:08:51 +0300

    Refactor loadMovieDetails for better readability

--- presentation/src/main/java/com/london/presentation/feature/search/details/movie/MovieDetailsViewModel.kt
+++ presentation/src/main/java/com/london/presentation/feature/search/details/movie/MovieDetailsViewModel.kt
@@ -42,25 +42,29 @@ class MovieDetailsViewModel @Inject constructor(
         // TODO: implement saving logic
     }
 
-    override fun onExpandClick() = updateState { copy(expanded = !expanded) }
+    override fun onExpandClick() {
+        updateState { copy(expanded = !expanded) }
+    }
 
-    override fun onMovieClick(movieId: Int) =
+    override fun onMovieClick(movieId: Int) {
         emitEffect(MovieDetailsEffect.MovieNavigation(movieId))
+    }
 
-    override fun onActorClick(actorId: Int) =
+    override fun onActorClick(actorId: Int) {
         emitEffect(MovieDetailsEffect.ActorNavigation(actorId))
+    }
 
-
-override fun onLoginClick() {
+    override fun onLoginClick() {
         emitEffect(MovieDetailsEffect.OnLoginNavigation)
     }
 
-    override fun onReviewsClick(movieId: Int, mediaNumber: Int) =
+    override fun onReviewsClick(movieId: Int, mediaNumber: Int) {
         emitEffect(MovieDetailsEffect.ReviewsNavigation(movieId, mediaNumber))
+    }
 
-
-    override fun onGenreClick(genreId: Int) =
+    override fun onGenreClick(genreId: Int) {
         emitEffect(MovieDetailsEffect.GenreNavigation(genreId))
+    }
 
     override fun onRetry() {
         updateState { copy(error = null) }
@@ -115,129 +119,66 @@ override fun onLoginClick() {
     }
 
     private fun loadMovieDetails(movieId: Int) {
-        tryToExecute(block = {
-            val movie = movieDetails.getMovieDetails(movieId)
-            val movieImages = movieDetails.getFirstTenMovieImagesUseCase(movieId)
-            val movieCast = movieDetails.getMovieCast(movieId)
-            Triple(movie, movieImages, movieCast)
-        }, onStart = { updateState { copy(isLoading = true) } }, onSuccess = { triple ->
-            val (details, images, cast) = triple
-            updateState {
-                copy(
-                    movieId = details.id,
-                    movieName = details.title,
-                    movieGenres = details.genresId,
-                    movieRating = details.voteAverage,
-                    movieDuration = details.runtime.toString(),
-                    releaseDate = details.releaseDate,
-                    movieOverview = details.overview,
-                    movieImages = images.ifEmpty { listOf(details.posterUrl) },
-                    actors = cast
-                )
-
-            }
-            addMovieToRecentViewed(
-                RecentViewed(
-                    id = details.id,
-                    imageUrl = details.posterUrl,
-                    type = MediaType.Movie,
-                    viewDate = System.currentTimeMillis()
-                )
-            )
-            addMovieToRecentWatched(
-                Movie(
-                    id = details.id,
-                    name = details.title,
-                    posterUrl = details.posterUrl,
-                    releaseYear = 2025,
-                    rating = 1,
-                    genreIds = details.genresId,
-                )
-            )
-        }, onError = { errorState ->
-            updateState { copy(error = errorState) }
-        }, onCompleted = {
-            updateState { copy(isLoading = false) }
-        })
-    }
-    private fun loadSimilarAndVideos(movieId: Int) {
-        tryToExecute(block = {
-            val similarMovies = movieDetails.getSimilarMovies(movieId)
-            val movieVideos = movieDetails.getMovieVideo(movieId)
-            val movieRating =
-                if (authenticationUseCase.isLoggedIn()) ratingUseCase.getRateAccountMovieStatesById(
-                    movieId
-                ) else 0
-            Triple(similarMovies, movieVideos, movieRating)
-        }, onSuccess = { (similarMovies, videos, movieRating) ->
-            updateState {
-                copy(
-                    similarMovies = similarMovies,
-                    movieVideo = videos.firstOrNull()?.videoUrl.orEmpty(),
-                    isRated = movieRating != 0 && state.value.isGuestUser.not()
-                )
-            }
-        }, onError = { errorState ->
-            updateState { copy(error = errorState) }
-        }, onCompleted = {
-            updateState { copy(isLoading = false) }
-        }
-        )
-    }
-
-    private suspend fun addMovieToRecentWatched(movie: Movie) =
-        manageRecentMovieWatchedUseCase.addMovieToRecentWatched(movie)
-
-    private suspend fun addMovieToRecentViewed(movie: RecentViewed) =
-        manageRecentViewedUseCase.addToRecentViewed(movie)
-
-    override fun onRetry() {
-        updateState { copy(error = null) }
-        loadMovieDetails(movieId)
-        loadSimilarAndVideos(movieId)
-    }
-
-    override fun onRateBottomSheetClick() {
-        tryToExecute(block = { authenticationUseCase.isLoggedIn() }, onSuccess = { isLoggedIn ->
-            if (isLoggedIn) updateState { copy(isRateBottomSheetVisible = isRateBottomSheetVisible.not()) }
-            else updateState {
-                copy(
-                    isGuestUserBottomSheetVisible = isGuestUserBottomSheetVisible.not(),
-                    isGuestUser = true
-                )
-            }
-        }, onError = { errorState ->
-            updateState { copy(error = errorState) }
-        }
-        )
-    }
-
-    override fun onLoginClick() = emitEffect(MovieDetailsEffect.OnLoginNavigation)
-
-    override fun onSelectRatingClick(rating: Int) {
         tryToExecute(
-            block = { ratingUseCase.addMovieRatingById(movieId, rating) },
-            onSuccess = {
+            block = {
+                val movie = movieDetails.getMovieDetails(movieId)
+                val movieImages = movieDetails.getFirstTenMovieImagesUseCase(movieId)
+                val movieCast = movieDetails.getMovieCast(movieId)
+                Triple(movie, movieImages, movieCast)
+            },
+            onStart = { updateState { copy(isLoading = true) } },
+            onSuccess = { triple ->
+                val (details, images, cast) = triple
                 updateState {
                     copy(
-                        selectedRating = rating,
-                        isRated = true,
-                        isRateBottomSheetVisible = false,
-                        isSuccessfullyRated = true
+                        movieId = details.id,
+                        movieName = details.title,
+                        movieGenres = details.genresId,
+                        movieRating = details.voteAverage,
+                        movieDuration = details.runtime.toString(),
+                        releaseDate = details.releaseDate,
+                        movieOverview = details.overview,
+                        movieImages = images.ifEmpty { listOf(details.posterUrl) },
+                        actors = cast
                     )
+
                 }
+                addMovieToRecentViewed(
+                    RecentViewed(
+                        id = details.id,
+                        imageUrl = details.posterUrl,
+                        type = MediaType.Movie,
+                        viewDate = System.currentTimeMillis()
+                    )
+                )
+                addMovieToRecentWatched(
+                    Movie(
+                        id = details.id,
+                        name = details.title,
+                        posterUrl = details.posterUrl,
+                        releaseYear = 2025,
+                        rating = 1,
+                        genreIds = details.genresId,
+                    )
+                )
             },
             onError = { errorState ->
-                updateState {
-                    copy(
-                        error = errorState,
-                        isSuccessfullyRated = false
-                    )
-                }
+                updateState { copy(error = errorState) }
             },
-            onCompleted = { updateState { copy(isLoading = false) } },
+            onCompleted = {
+                updateState { copy(isLoading = false) }
+            }
         )
     }
+
+    private suspend fun addMovieToRecentWatched(movie: Movie) {
+        manageRecentMovieWatchedUseCase.addMovieToRecentWatched(movie)
+    }
+
+    private suspend fun addMovieToRecentViewed(movie: RecentViewed) {
+        manageRecentViewedUseCase.addToRecentViewed(movie)
+    }
+
     private fun loadSimilarAndVideos(movieId: Int) {
         tryToExecute(
             block = {
