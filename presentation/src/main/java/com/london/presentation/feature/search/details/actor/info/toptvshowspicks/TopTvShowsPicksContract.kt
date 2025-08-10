d50b01381b29e71641bfae051bf2bad6d115735f
Abdulrahman Mohammad Ragab <76666355+Abdulrahman1890@users.noreply.github.com>
2025-08-10 05:56:03 +0300

    Hotfix/rearrange presentation packages (#595)
    * fix: rearrange presentation packages
    
    * fix: rearrange presentation packages
    
    * fix: agp version

--- presentation/src/main/java/com/london/presentation/feature/search/details/actor/info/toptvshowspicks/TopTvShowsPicksContract.kt
+++
@@ -1,8 +0,0 @@
-package com.london.presentation.feature.search.details.actor.info.toptvshowspicks
-
-interface TopTvShowsPicksContract {
-    fun onSaveTvShow(tvShowId: Int)
-    fun onTvShowClicked(tvShowId: Int)
-    fun onBack()
-    fun onRetry()
-}
