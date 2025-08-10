3d4d106cb615d9d938287ecad5ed5e5d93ea11bb
Yusuf Nasser <yusufnassereng@gmail.com>
2025-08-10 06:46:30 +0300

    refactor: remove RatingData class

--- presentation/src/main/java/com/london/presentation/feature/accountinfo/rating/RatingData.kt
+++
@@ -1,8 +0,0 @@
-package com.london.presentation.feature.accountinfo.rating
-
-import com.london.domain.entity.RatedMedia
-
-data class RatingData(
-    val ratedMovies: List<RatedMedia>,
-    val ratedTvShows: List<RatedMedia>
-)
