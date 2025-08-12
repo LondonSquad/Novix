b1674e75f571b8fe6ecb3e5b517bfe71e8529a78
Yusuf Nasser <yusufnassereng@gmail.com>
2025-08-12 21:12:57 +0300

    refactor: change toBookmarkUiLists to map List instead of Flow<PagingData>

--- presentation/src/main/java/com/london/presentation/shared/bookmarkSheet/mapper.kt
+++ presentation/src/main/java/com/london/presentation/shared/bookmarkSheet/mapper.kt
@@ -1,18 +1,12 @@
 package com.london.presentation.shared.bookmarkSheet
 
-import androidx.paging.PagingData
-import androidx.paging.map
 import com.london.domain.entity.MovieList
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
 
-fun Flow<PagingData<MovieList>>.toBookmarkUiLists(): Flow<PagingData<BookmarkUiList>> =
-    map { pagingData ->
-        pagingData.map { list ->
-            BookmarkUiList(
-                id = list.id,
-                name = list.name,
-                itemCount = list.moviesCount.toUShort()
-            )
-        }
+fun List<MovieList>.toBookmarkUiLists(): List<BookmarkUiList> =
+    map { movieList ->
+        BookmarkUiList(
+            id = movieList.id,
+            name = movieList.name,
+            itemCount = movieList.moviesCount.toUShort()
+        )
     }
