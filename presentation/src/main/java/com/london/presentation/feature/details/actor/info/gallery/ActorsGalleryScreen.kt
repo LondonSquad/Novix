bd30c1fee52d44992d3a6953801dcecf943b9454
Abdulrahman1890 <Abdulrahmanmm2011@hotmail.com>
2025-08-13 05:08:34 +0300

    fix: separate gallery body to another function

--- presentation/src/main/java/com/london/presentation/feature/details/actor/info/gallery/ActorsGalleryScreen.kt
+++ presentation/src/main/java/com/london/presentation/feature/details/actor/info/gallery/ActorsGalleryScreen.kt
@@ -80,33 +80,40 @@ private fun Content(
             if (uiState.isLoading) {
                 CircularLoading(modifier = Modifier.align(Alignment.Center))
             } else {
-                LazyVerticalGrid(
-                    columns = GridCells.Adaptive(minSize = 104.dp),
-                    horizontalArrangement = Arrangement.spacedBy(8.dp),
-                    verticalArrangement = Arrangement.spacedBy(12.dp),
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(NovixTheme.colors.surface)
-                ) {
-                    items(uiState.images) { imageUrl ->
-                        ImageView(
-                            model = imageUrl,
-                            contentDescription = stringResource(R.string.actor_photos),
-                            modifier = Modifier
-                                .size(width = 104.dp, height = 101.dp)
-                                .border(
-                                    width = 1.dp,
-                                    color = NovixTheme.colors.stroke,
-                                    shape = RoundedCornerShape(12.dp)
-                                )
-                                .clip(RoundedCornerShape(12.dp)),
-                            contentScale = ContentScale.Crop,
-                            loadingContent = { CircularLoading() },
-                            errorContent = { ErrorImage(NovixTheme.isThemeDark) }
-                        )
-                    }
-                }
+                GalleryContent(uiState = uiState)
             }
+        }
+    }
+}
+
+@Composable
+private fun GalleryContent(
+    uiState: ActorsGalleryUiState
+){
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(minSize = 104.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+        modifier = Modifier
+            .fillMaxSize()
+            .background(NovixTheme.colors.surface)
+    ) {
+        items(uiState.images) { imageUrl ->
+            ImageView(
+                model = imageUrl,
+                contentDescription = stringResource(R.string.actor_photos),
+                modifier = Modifier
+                    .size(width = 104.dp, height = 101.dp)
+                    .border(
+                        width = 1.dp,
+                        color = NovixTheme.colors.stroke,
+                        shape = RoundedCornerShape(12.dp)
+                    )
+                    .clip(RoundedCornerShape(12.dp)),
+                contentScale = ContentScale.Crop,
+                loadingContent = { CircularLoading() },
+                errorContent = { ErrorImage(NovixTheme.isThemeDark) }
+            )
         }
     }
 }
