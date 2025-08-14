edb5de85ee87b79c9ddf974b1b0832b1ddb18a54
Asmaa.Khaled <asmaa.kh.gomaa@gmail.com>
2025-08-15 00:56:12 +0300

    refactor: Remove statusBarsPadding from DefaultAppTopBar

--- presentation/src/main/java/com/london/presentation/shared/DefaultAppTopBar.kt
+++ presentation/src/main/java/com/london/presentation/shared/DefaultAppTopBar.kt
@@ -2,7 +2,6 @@ package com.london.presentation.shared
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -16,8 +15,7 @@ fun DefaultAppTopBar(
     TopBar(
         modifier = Modifier
             .fillMaxWidth()
-            .statusBarsPadding()
-            .padding(start = 16.dp, end = 16.dp),
+            .padding(start = 16.dp, end = 16.dp, top = 12.dp),
         title = title,
         onBackClick = onBack
     )
