1657aafd11392d7c1aebb57126522a4e5405df4c
Abdulrahman Mohammad Ragab <76666355+Abdulrahman1890@users.noreply.github.com>
2025-08-14 03:43:20 +0300

    fix: refactor top movies picks (#609)

--- presentation/src/main/java/com/london/presentation/feature/details/actor/info/topmoviespicks/TopMoviesPicksContract.kt
+++ presentation/src/main/java/com/london/presentation/feature/details/actor/info/topmoviespicks/TopMoviesPicksContract.kt
@@ -1,8 +1,8 @@
 package com.london.presentation.feature.details.actor.info.topmoviespicks
 
 interface TopMoviesPicksContract {
-    fun onSaveMovie(movieId: Int)
-    fun onMovieClicked(movieId: Int)
-    fun onBack()
-    fun onRetry()
+    fun onBackClick()
+    fun onRetryClick()
+    fun onSaveMovieClick(movieId: Int)
+    fun onMovieClick(movieId: Int)
 }
