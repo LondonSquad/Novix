d703ac64962cc6806eb41c3ec269cfe858cdce20
ahmeddsamii <154278495+ahmeddsamii@users.noreply.github.com>
2025-08-14 16:02:45 +0300

    feat: Refactor HomeViewModel to use reactive flows and made it well s… (#629)
    * feat: Refactor HomeViewModel to use reactive flows and made it well structured
    
    * Refactor: Remove unused modifier parameters in HomeScreen
    
    * fix: Add missing MediaType to TopRatedMedia in tests
    
    ---------
    
    Co-authored-by: london-bot[bot] <220529784+london-bot[bot]@users.noreply.github.com>

--- presentation/src/main/java/com/london/presentation/feature/home/HomeScreenContract.kt
+++ presentation/src/main/java/com/london/presentation/feature/home/HomeScreenContract.kt
@@ -3,26 +3,14 @@ package com.london.presentation.feature.home
 import com.london.presentation.utils.MovieGenre
 
 interface HomeScreenContract {
-    fun onRetry()
+    fun onRetryClick()
     fun onTopRatedClick()
     fun onMovieClick(id: Int)
     fun onTvShowClick(id: Int)
     fun onContinueWatchingClick()
-    fun onTrendingMoviesCardClicked()
-    fun onTrendingActorsCardClicked()
-    fun onTrendingTvShowsCardClicked()
+    fun onTrendingMoviesCardClick()
+    fun onTrendingActorsCardClick()
+    fun onTrendingTvShowsCardClick()
     fun onMovieGenreSelect(genre: MovieGenre)
+    fun loadUpcomingMoviesClick(categoryId: Int?)
 }
-
-fun defaultHomeScreenContract() = object : HomeScreenContract {
-    override fun onRetry() {}
-    override fun onTopRatedClick() {}
-    override fun onMovieClick(id: Int) {}
-    override fun onTvShowClick(id: Int) {}
-    override fun onContinueWatchingClick() {}
-    override fun onTrendingMoviesCardClicked() {}
-    override fun onTrendingActorsCardClicked() {}
-    override fun onTrendingTvShowsCardClicked() {}
-    override fun onMovieGenreSelect(genre: MovieGenre) {}
-}
-
