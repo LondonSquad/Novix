5853447625435f9dd15ee705395a9d3dd4ab436f
Abdulrahman Mohammad Ragab <76666355+Abdulrahman1890@users.noreply.github.com>
2025-08-14 03:43:20 +0300

    fix: refactor top movies picks (#609)

--- presentation/src/main/java/com/london/presentation/feature/details/actor/info/topmoviespicks/TopMoviesPicksViewModel.kt
+++ presentation/src/main/java/com/london/presentation/feature/details/actor/info/topmoviespicks/TopMoviesPicksViewModel.kt
@@ -10,56 +10,43 @@ import javax.inject.Inject
 
 @HiltViewModel
 class TopMoviesPicksViewModel @Inject constructor(
-    private val getActorMoviePicksById: GetActorMoviePicksByIdUseCase,
     savedStateHandle: SavedStateHandle,
+    private val getActorMoviePicksById: GetActorMoviePicksByIdUseCase,
 ) : BaseViewModel<TopMoviesPicksUiState, TopMoviesPicksEffect>(TopMoviesPicksUiState()),
     TopMoviesPicksContract {
 
     private val args = savedStateHandle.getArgs<Screen.ActorTopMoviesPicksDetails>()
-    val actorId = args?.actorId ?: 0
+    private val actorId = args?.actorId ?: 0
 
     init {
-        if (actorId != 0) {
-            getActorMoviePicksData()
-        }
+        getActorMoviePicksData()
     }
 
-    private fun getActorMoviePicksData() {
 
-        tryToExecute(
-            block = { getActorMoviePicksById.invoke(actorId) },
-            onStart = { updateState { copy(isLoading = true) } },
-            onSuccess = { actorMovieDetails ->
-                updateState {
-                    copy(
-                        movieDetails = actorMovieDetails,
-                        isSaved = isSaved,
-                        backdropPath = backdropPath,
-                    )
-                }
-            },
-            onError = { errorState ->
-                updateState { copy(errorState = errorState) }
-            },
-            onCompleted = { updateState { copy(isLoading = false) } },
-            checkSuccess = { actorId != 0 },
-        )
-    }
-
-    override fun onRetry(){
+    override fun onRetryClick() {
         updateState { copy(errorState = null) }
         getActorMoviePicksData()
     }
 
-    override fun onSaveMovie(movieId: Int) {
-        updateState { copy(isSaved = isSaved) }
+    override fun onSaveMovieClick(movieId: Int) {
+        // TODO: Handle save click
     }
 
-    override fun onMovieClicked(movieId: Int) {
-        emitEffect(TopMoviesPicksEffect.NavigationToMovieDetails(movieId))
+    override fun onMovieClick(movieId: Int) {
+        emitEffect(TopMoviesPicksEffect.MovieDetailsNavigation(movieId))
     }
 
-    override fun onBack() {
-        emitEffect(TopMoviesPicksEffect.NavigateBack)
+    override fun onBackClick() {
+        emitEffect(TopMoviesPicksEffect.BackNavigation)
+    }
+
+    private fun getActorMoviePicksData() {
+        tryToExecute(
+            block = { getActorMoviePicksById.invoke(actorId) },
+            onStart = { updateState { copy(isLoading = true) } },
+            onSuccess = { actorMovieDetails -> updateState { copy(movieDetails = actorMovieDetails) } },
+            onError = { errorState -> updateState { copy(errorState = errorState) } },
+            onCompleted = { updateState { copy(isLoading = false) } },
+        )
     }
 }
