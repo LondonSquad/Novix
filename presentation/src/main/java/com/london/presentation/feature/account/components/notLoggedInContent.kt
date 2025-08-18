81305be0560bfb415c5db2d338f4b5caed79c0dd
Bassant Mohammed <bassant.22230@gmail.com>
2025-08-18 22:04:28 +0300

    refactor: padding and image size

--- presentation/src/main/java/com/london/presentation/feature/account/components/notLoggedInContent.kt
+++ presentation/src/main/java/com/london/presentation/feature/account/components/notLoggedInContent.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -38,6 +39,7 @@ fun NotLoggedInContent(
                 painter = if (isDarkMode) R.drawable.user_person_dark.painter
                 else R.drawable.user_person_light.painter,
                 contentDescription = null,
+                Modifier.size(128.dp)
             )
         }
 
@@ -47,7 +49,9 @@ fun NotLoggedInContent(
                 style = NovixTheme.typography.body.small,
                 color = NovixTheme.colors.body,
                 textAlign = TextAlign.Center,
-                modifier = Modifier.padding(horizontal = 48.dp, vertical = 24.dp)
+                modifier = Modifier
+                    .padding(horizontal = 48.dp)
+                    .padding(bottom = 24.dp)
             )
         }
 
