3a1282bd7b12d207986d922c5a34b89dbf259d9b
ahmbd <ahmbdelkhalek@gmail.com>
2025-08-20 13:18:39 +0300

    Refactor: Remove inset shadow from PrimaryButton

--- designSystem/src/main/java/com/london/designsystem/component/button/PrimaryButton.kt
+++ designSystem/src/main/java/com/london/designsystem/component/button/PrimaryButton.kt
@@ -1,7 +1,5 @@
 package com.london.designsystem.component.button
 
-import android.annotation.SuppressLint
-import android.graphics.BlurMaskFilter
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
@@ -17,13 +15,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawWithCache
-import androidx.compose.ui.graphics.BlendMode
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Paint
-import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.london.designsystem.R
 import com.london.designsystem.theme.NovixTheme
@@ -45,8 +37,7 @@ fun PrimaryButton(
         enabled = enabled,
         modifier = modifier
             .heightIn(48.dp)
-            .defaultMinSize(minWidth = 52.dp)
-            .insetShadow(),
+            .defaultMinSize(minWidth = 52.dp),
         shape = RoundedCornerShape(12.dp),
         colors = ButtonDefaults.buttonColors(
             containerColor = NovixTheme.colors.primary,
@@ -55,7 +46,6 @@ fun PrimaryButton(
             disabledContentColor = NovixTheme.colors.onPrimaryHint
         ),
         contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
-        elevation = ButtonDefaults.buttonElevation(defaultElevation = 2.dp)
     ) {
 
         if (hasLabel && text != null) {
@@ -87,44 +77,6 @@ fun PrimaryButton(
         }
     }
 }
-
-@SuppressLint("SuspiciousModifierThen")
-private fun Modifier.insetShadow(
-    color: Color = Color(0x1F0D0608),
-    offsetX: Dp = 2.dp,
-    offsetY: Dp = 4.dp,
-    blurRadius: Dp = 12.dp,
-    cornerRadius: Dp = 12.dp
-) = this.then(
-    drawWithCache {
-        val paint = Paint().apply {
-            this.color = color
-            this.blendMode = BlendMode.SrcOver
-        }
-
-        val pxBlurRadius = blurRadius.toPx()
-        val pxCornerRadius = cornerRadius.toPx()
-
-        onDrawWithContent {
-            drawContent()
-            drawIntoCanvas { canvas ->
-                canvas.drawRoundRect(
-                    left = 0f,
-                    top = 0f,
-                    right = size.width,
-                    bottom = size.height,
-                    radiusX = pxCornerRadius,
-                    radiusY = pxCornerRadius,
-                    paint = paint.apply {
-                        asFrameworkPaint().apply {
-                            maskFilter = BlurMaskFilter(pxBlurRadius, BlurMaskFilter.Blur.NORMAL)
-                        }
-                    }
-                )
-            }
-        }
-    }
-)
 
 @ThemePreviews
 @Composable
