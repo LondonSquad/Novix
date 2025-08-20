e69bb1c4e033f59a3288fc566492239a6c89e5e8
Asmaa.Khaled <asmaa.kh.gomaa@gmail.com>
2025-08-20 11:57:35 +0300

    refactor: Standardize `onBack` and `onManageBookmark` method names

--- presentation/src/main/java/com/london/presentation/feature/category/movie/MovieCategoryScreen.kt
+++ presentation/src/main/java/com/london/presentation/feature/category/movie/MovieCategoryScreen.kt
@@ -1,25 +1,21 @@
 package com.london.presentation.feature.category.movie
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
-import com.london.designsystem.component.TopBar
 import com.london.designsystem.theme.ThemePreviews
 import com.london.presentation.R
-import com.london.presentation.shared.MediaLazyPagingGrid
+import com.london.presentation.shared.DefaultAppTopBar
 import com.london.presentation.shared.bookmarkSheet.BookmarkBottomSheet
 import com.london.presentation.shared.buildscreen.BuildScreen
+import com.london.presentation.shared.container.MediaLazyVerticalGrid
 import com.london.presentation.utils.Listen
 import com.london.presentation.utils.isLoading
 
@@ -54,49 +50,38 @@ private fun Content(
 ) {
 
     val moviesLazyList = state.moviesFlow.collectAsLazyPagingItems()
+
     BuildScreen(
-        onBack = contract::onBack,
+        onBack = contract::onBackClick,
         isLoading = moviesLazyList.isLoading(),
         isError = moviesLazyList.loadState.refresh is LoadState.Error,
         onRetry = moviesLazyList::refresh,
         emptyLayoutMessage = R.string.there_is_no_items_for_this_genre,
         emptyLayoutImage = R.drawable.empty
     ) {
+        MediaLazyVerticalGrid(
+            pagingItems = moviesLazyList,
+            imageUrl = { it.posterUrl },
+            name = { it.name },
+            hasSaveIcon = true,
+            onSaveClick = { contract.onManageBookmarkClick(it.id) },
+            isItemSaved = { false },
+            onNavigateToMovie = { id -> contract.onMovieClick(id) },
+            topBar = {
+                DefaultAppTopBar(
+                    title = stringResource(state.genre.stringResId),
+                    onBackClick = contract::onBackClick
+                )
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
 
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(top = 12.dp)
-        ) {
-            TopBar(
-                title = stringResource(
-                    state.genre.stringResId
-                ),
-                onBackClick = contract::onBack,
-                modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 12.dp)
-            )
+        BookmarkBottomSheet(
+            onSheetDismiss = contract::onBookmarkSheetDismiss,
+            isSheetVisible = state.isBookmarkSheetVisible,
+            bookmarkedMovieId = state.bookmarkedMovieId
+        )
 
-            MediaLazyPagingGrid(
-                pagingFlow = moviesLazyList,
-                onItemClick = { contract.onMovieClick(it.id) },
-                getImageUrl = { it.posterUrl },
-                getTitle = { "${it.name} movie img" },
-                modifier = Modifier
-                    .weight(1f)
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp),
-                onSaveClick = { contract.onManageBookmarkClicked(it.id) },
-                isItemSaved = { false },
-                hasSaveIcon = true
-            )
-
-            BookmarkBottomSheet(
-                onSheetDismiss = contract::onBookmarkSheetDismiss,
-                isSheetVisible = state.isBookmarkSheetVisible,
-                bookmarkedMovieId = state.bookmarkedMovieId
-            )
-
-        }
     }
 }
 
@@ -108,8 +93,8 @@ private fun MoviesByCategoryContentPreview() {
         contract = object : MovieCategoryContract {
             override fun onMovieClick(movieId: Int) {}
             override fun onBookmarkSheetDismiss() {}
-            override fun onManageBookmarkClicked(movieId: Int) {}
-            override fun onBack() {}
+            override fun onManageBookmarkClick(movieId: Int) {}
+            override fun onBackClick() {}
         },
     )
 }
